26ba9e9a97175583766f1a6577451e13753d507d
downanup <downanup@ff461055-cf1a-0410-99e4-fa365203d4c3>
2005-12-26 20:45:27 +0000

    Separted between FRONTEND and BACKEND. NDO DB Backend works now :-) (with the features implemented there, wich is only a simple Host State at the momen)
    git-svn-id: file:///root/orig/nagvis/trunk@79 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/index.php
+++ nagvis/index.php
@@ -10,13 +10,21 @@
 ## please see attached "LICENCE" file	                                ##
 ##########################################################################
 
+##########################################################################
+## Code Format: Vars: wordWordWord										##
+##		   	 Classes: wordwordword                                      ##
+##		     Objects: WORDWORDWORD                                      ##
+## Please use TAB (Tab Size: 4 Spaces) to format the code               ##
+##########################################################################
+
 include("./etc/config.inc.php");
 include("./includes/classes/class.Graphic.php");
 include("./includes/classes/class.CheckIt.php");
 include("./includes/classes/class.ReadFiles.php");
+// The Backend
 include("./includes/classes/class.CheckState_".$StateClass.".php");
 
-$CheckIt = new checkit();
+$CHECKIT = new checkit();
 
 // Browser ermitteln.
 unset($browser);
@@ -25,29 +33,29 @@ $browser = $_SERVER['HTTP_USER_AGENT'];
 // Map festlegen.
 if(isset($_GET['map'])) {
     $map = $_GET['map'];
-    $CheckIt->check_map_isreadable();
+    $CHECKIT->check_map_isreadable();
 } else {
     $map = $maps[0];
-	$CheckIt->check_map_isreadable();
+	$CHECKIT->check_map_isreadable();
 }
 
-$nagvis = new NagVis();
-$readfile = new readFile();
+$FRONTEND = new frontend();
+$READFILE = new readFile();
 $rotateUrl = "";
 
 // check-stuff
-$CheckIt->check_user();
-$CheckIt->check_gd();
-$CheckIt->check_cgipath();
-$CheckIt->check_wuibash();
-$CheckIt->check_rotate();
+$CHECKIT->check_user();
+$CHECKIT->check_gd();
+$CHECKIT->check_cgipath();
+$CHECKIT->check_wuibash();
+$CHECKIT->check_rotate();
 
 /*
 FIXME: Ha: Can we delete this permanent???
 
 // Prüfen ob Rotate-Modus eingeschaltet ist.
 if($RotateMaps == "1") {
-    $mapNumber = $nagvis->mapCount($map);
+    $mapNumber = $FRONTEND->mapCount($map);
     $map = $maps[$mapNumber];
     $rotateUrl = " URL=index.php?map=".$map;
 }  
@@ -56,30 +64,32 @@ if($RotateMaps == "1") {
 //Prüfen ob *.cfg-Datei vorhanden ist und dann einlesen.
 //FIXME sollte erst NACH den ganzen Plausis gemacht werden
 if(file_exists($cfgFolder.$map.".cfg")) {
-    $mapCfg = $readfile->readNagVisCfg($map);
+    $mapCfg = $READFILE->readNagVisCfg($map);
     $allowed_users = explode(",",trim($mapCfg[1]['allowed_user']));
     $map_image_array = explode(",",trim($mapCfg[1]['map_image']));
     $map_image=$map_image_array[0];
 }
-$nagvis->openSite($rotateUrl);
+$FRONTEND->openSite($rotateUrl);
 
-$CheckIt->check_permissions();
-$CheckIt->check_map_isreadable();
-$CheckIt->check_mapimg();
-$CheckIt->check_langfile();
+$CHECKIT->check_permissions();
+$CHECKIT->check_map_isreadable();
+$CHECKIT->check_mapimg();
+$CHECKIT->check_langfile();
 
 // Prüfen ob Header eingeschaltet ist und bei bedarf erzeugen.
 if ($Header == "1") {
-	$Menu = $readfile->readMenu();
-	$nagvis->makeHeaderMenu($Menu);
+	$Menu = $READFILE->readMenu();
+	$FRONTEND->makeHeaderMenu($Menu);
 }
 
 //Map als Hintergrundbild erzeugen
-$nagvis->printMap($map);
+$FRONTEND->printMap($map);
 
-$checkstate = new checkState();
+//Load and initialise the backend
+$BACKEND = new backend();
+$BACKEND->backendInitialize();
+
 $countStates = count($mapCfg)-1;
-
 // FIXME: HA: The following line looks "gemurkst" ;-)
 $arrayPos="2";
 
@@ -113,7 +123,7 @@ for($x="1";$x<=$countStates;$x++) {
     // Handle all objects of type "map"
     if($mapCfg[$arrayPos]['type'] == 'map') {
 		if(file_exists($cfgFolder.$mapCfg[$arrayPos]['name'].'.cfg')) {
-			$allMapCfgState = $readfile->readNagVisCfg($mapCfg[$arrayPos]['name']);
+			$allMapCfgState = $READFILE->readNagVisCfg($mapCfg[$arrayPos]['name']);
 			
 			/*
 				FIXME: Ha: Can we delte this permanent???
@@ -134,7 +144,7 @@ for($x="1";$x<=$countStates;$x++) {
 	
 				$allowed_types = array(host,service,hostgroup,servicegroup);
 				if(in_array($mapCfgState['type'], $allowed_types)){
-					$stateState = $checkstate->checkStates($mapCfgState['type'],$mapCfgState['name'],$mapCfgState['recognize_services'],$mapCfgState['service_description'],0,$CgiPath,$CgiUser);
+					$stateState = $BACKEND->checkStates($mapCfgState['type'],$mapCfgState['name'],$mapCfgState['recognize_services'],$mapCfgState['service_description'],0,$CgiPath,$CgiUser);
 					$mapState[] = $stateState['State'];
 				}
 				$countStatesState++;
@@ -143,93 +153,93 @@ for($x="1";$x<=$countStates;$x++) {
 			if(in_array("DOWN", $mapState) || in_array("CRITICAL", $mapState)){
 				$state['State'] = "CRITICAL";
 				$state['Output'] = "State of parent Map is CRITICAL or DOWN";
-				$Icon = $checkstate->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
+				$Icon = $BACKEND->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
 			}elseif(in_array("WARNING", $mapState)){
 				$state['State'] = "WARNING";
 				$state['Output'] = "State of parent Map is  WARNING";
-				$Icon = $checkstate->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
+				$Icon = $BACKEND->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
 			}else{
 				$state['State'] = "OK";
 				$state['Output'] = "State of parent map is OK or UP";
-				$Icon = $checkstate->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
+				$Icon = $BACKEND->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
 			}
 		} else {
 			$state['State'] = "OK";
 			$state['Output'] = "Map not readable";
 		}
 	
-		$IconPosition = $checkstate->fixIconPosition($Icon,$mapCfg[$arrayPos]['x'],$mapCfg[$arrayPos]['y']);
-		$nagvis->site[] = $IconPosition;
-		$Box = $nagvis->infoBox($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['service_description'],$state);
-		$nagvis->site[] = '<A HREF="./index.php?map='.$mapCfg[$arrayPos]['name'].'" TARGET="_self"><IMG SRC='.$iconHTMLBaseFolder.$Icon.' '.$Box.'; BORDER="0"></A>';
-		$nagvis->site[] = "</DIV>";
+		$IconPosition = $BACKEND->fixIconPosition($Icon,$mapCfg[$arrayPos]['x'],$mapCfg[$arrayPos]['y']);
+		$FRONTEND->site[] = $IconPosition;
+		$Box = $FRONTEND->infoBox($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['service_description'],$state);
+		$FRONTEND->site[] = '<A HREF="./index.php?map='.$mapCfg[$arrayPos]['name'].'" TARGET="_self"><IMG SRC='.$iconHTMLBaseFolder.$Icon.' '.$Box.'; BORDER="0"></A>';
+		$FRONTEND->site[] = "</DIV>";
     
 	// Handle objects of type "textbox"
     }elseif($mapCfg[$arrayPos]['type'] == 'textbox') {
-		$TextBox = $checkstate->TextBox($mapCfg[$arrayPos]['x'],$mapCfg[$arrayPos]['y'],$mapCfg[$arrayPos]['w'],$mapCfg[$arrayPos]['text']);
-		$nagvis->site[] = $TextBox;		
+		$TextBox = $BACKEND->TextBox($mapCfg[$arrayPos]['x'],$mapCfg[$arrayPos]['y'],$mapCfg[$arrayPos]['w'],$mapCfg[$arrayPos]['text']);
+		$FRONTEND->site[] = $TextBox;		
 	
 	// Handle objects of type "line" and take care about the differnt line_types
 	}elseif(isset($mapCfg[$arrayPos]['line_type'])) {
 	    if($mapCfg[$arrayPos]['line_type'] == '10' || $mapCfg[$arrayPos]['line_type'] == '11'){
-			$state = $checkstate->checkStates($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['recognize_services'],$mapCfg[$arrayPos]['service_description'],0,$CgiPath,$CgiUser);
+			$state = $BACKEND->checkStates($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['recognize_services'],$mapCfg[$arrayPos]['service_description'],0,$CgiPath,$CgiUser);
 			list($x_from,$x_to) = explode(",", $mapCfg[$arrayPos]['x']);
 			list($y_from,$y_to) = explode(",", $mapCfg[$arrayPos]['y']);
 			$x_middle = middle($x_from,$x_to);
 			$y_middle = middle($y_from,$y_to);
-			$IconPosition = $checkstate->fixIconPosition('20x20.gif',$x_middle,$y_middle);
-			$Box = $nagvis->infoBox($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['service_description'],$state);
-			$nagvis->site[] = $IconPosition;
-			$nagvis->site[] = $link;
-			$nagvis->site[] = '<img src=iconsets/20x20.gif '.$Box.';></A>';
-			$nagvis->site[] = '</div>';
+			$IconPosition = $BACKEND->fixIconPosition('20x20.gif',$x_middle,$y_middle);
+			$Box = $FRONTEND->infoBox($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['service_description'],$state);
+			$FRONTEND->site[] = $IconPosition;
+			$FRONTEND->site[] = $link;
+			$FRONTEND->site[] = '<img src=iconsets/20x20.gif '.$Box.';></A>';
+			$FRONTEND->site[] = '</div>';
 
 	    } elseif($mapCfg[$arrayPos]['line_type'] == '20'){
 			list($host_name_from,$host_name_to) = explode(",", $mapCfg[$arrayPos]['name']);
 			list($service_description_from,$service_description_to) = explode(",", $mapCfg[$arrayPos]['service_description']);
-			$state_from = $checkstate->checkStates($mapCfg[$arrayPos]['type'],$host_name_from,$mapCfg[$arrayPos]['recognize_services'],$service_description_from,1,$CgiPath,$CgiUser);
-			$state_to = $checkstate->checkStates($mapCfg[$arrayPos]['type'],$host_name_to,$mapCfg[$arrayPos]['recognize_services'],$service_description_to,2,$CgiPath,$CgiUser);
+			$state_from = $BACKEND->checkStates($mapCfg[$arrayPos]['type'],$host_name_from,$mapCfg[$arrayPos]['recognize_services'],$service_description_from,1,$CgiPath,$CgiUser);
+			$state_to = $BACKEND->checkStates($mapCfg[$arrayPos]['type'],$host_name_to,$mapCfg[$arrayPos]['recognize_services'],$service_description_to,2,$CgiPath,$CgiUser);
 
 			list($x_from,$x_to) = explode(",", $mapCfg[$arrayPos]['x']);
 			list($y_from,$y_to) = explode(",", $mapCfg[$arrayPos]['y']);
 			// From
 			$x_middle = middle2($x_from,$x_to);
 			$y_middle = middle2($y_from,$y_to);
-			$IconPosition = $checkstate->fixIconPosition('20x20.gif',$x_middle,$y_middle);
-			$Box = $nagvis->infoBox($mapCfg[$arrayPos]['type'],$host_name_from,$service_description_from,$state_from);
-			$nagvis->site[] = $IconPosition;
-			$nagvis->site[] = $link;
-			$nagvis->site[] = '<img src= iconsets/20x20.gif '.$Box.';></A>';
-			$nagvis->site[] = '</div>';
+			$IconPosition = $BACKEND->fixIconPosition('20x20.gif',$x_middle,$y_middle);
+			$Box = $FRONTEND->infoBox($mapCfg[$arrayPos]['type'],$host_name_from,$service_description_from,$state_from);
+			$FRONTEND->site[] = $IconPosition;
+			$FRONTEND->site[] = $link;
+			$FRONTEND->site[] = '<img src= iconsets/20x20.gif '.$Box.';></A>';
+			$FRONTEND->site[] = '</div>';
 			// To
 			$x_middle = middle2($x_to,$x_from);
 			$y_middle = middle2($y_to,$y_from);
-			$IconPosition = $checkstate->fixIconPosition('20x20.gif',$x_middle,$y_middle);
-			$Box = $nagvis->infoBox($mapCfg[$arrayPos]['type'],$host_name_to,$service_description_to,$state_to);
-			$nagvis->site[] = $IconPosition;
-			$nagvis->site[] = $link;
-			$nagvis->site[] = '<img src=iconsets/20x20.gif '.$Box.';></A>';
-			$nagvis->site[] = '</div>';
+			$IconPosition = $BACKEND->fixIconPosition('20x20.gif',$x_middle,$y_middle);
+			$Box = $FRONTEND->infoBox($mapCfg[$arrayPos]['type'],$host_name_to,$service_description_to,$state_to);
+			$FRONTEND->site[] = $IconPosition;
+			$FRONTEND->site[] = $link;
+			$FRONTEND->site[] = '<img src=iconsets/20x20.gif '.$Box.';></A>';
+			$FRONTEND->site[] = '</div>';
 		}	
 
 	// Handle all the other types (hosts, services, hostgroups, servicegroups)
 	}elseif(!isset($mapCfg[$arrayPos]['line_type'])) {
-		$state = $checkstate->checkStates($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['recognize_services'],$mapCfg[$arrayPos]['service_description'],0,$CgiPath,$CgiUser);
+		$state = $BACKEND->checkStates($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['recognize_services'],$mapCfg[$arrayPos]['service_description'],0,$CgiPath,$CgiUser);
 
-		$Icon = $checkstate->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
-		$IconPosition = $checkstate->fixIconPosition($Icon,$mapCfg[$arrayPos]['x'],$mapCfg[$arrayPos]['y']);
-		$nagvis->site[] = $IconPosition;
+		$Icon = $BACKEND->fixIcon($state,$mapCfg,$arrayPos,$defaultIcons,$mapCfg[$arrayPos]['type']);
+		$IconPosition = $BACKEND->fixIconPosition($Icon,$mapCfg[$arrayPos]['x'],$mapCfg[$arrayPos]['y']);
+		$FRONTEND->site[] = $IconPosition;
 		
-		$Box = $nagvis->infoBox($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['service_description'],$state);
-		$nagvis->site[] = $link;
-		$nagvis->site[] = '<IMG SRC='.$iconHTMLBaseFolder.$Icon.' '.$Box.';></A>';
-		$nagvis->site[] = "</DIV>";
+		$Box = $FRONTEND->infoBox($mapCfg[$arrayPos]['type'],$mapCfg[$arrayPos]['name'],$mapCfg[$arrayPos]['service_description'],$state);
+		$FRONTEND->site[] = $link;
+		$FRONTEND->site[] = '<IMG SRC='.$iconHTMLBaseFolder.$Icon.' '.$Box.';></A>';
+		$FRONTEND->site[] = "</DIV>";
 	}
     $arrayPos++;
 
 //~End of main loop
 }
-$nagvis->closeSite();
-$nagvis->printSite();
+$FRONTEND->closeSite();
+$FRONTEND->printSite();
 
 ?>
