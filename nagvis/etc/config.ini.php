266c6c46a95737ad273035fa387aae912c651432
downanup <downanup@ff461055-cf1a-0410-99e4-fa365203d4c3>
2006-05-31 09:50:25 +0000

    - Removed "checkconfig" Option - Updatet Version
    git-svn-id: file:///root/orig/nagvis/trunk@307 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/etc/config.ini.php
+++ nagvis/etc/config.ini.php
@@ -1,5 +1,5 @@
 ; <? return 1; ?>
-; the line above is to prevent 
+; the line above is to prevent
 ; viewing this file from web.
 ; DON'T REMOVE IT!
 
@@ -12,32 +12,30 @@
 ; select backend to use (ndomy,html)
 ; html -> pulls the data from the Nagios Webinterface (slow and unreliable but
 ;		  easy to use)
-; ndomy -> gets the data from a Nagios NDO MySQL Database (fast an reliable 
+; ndomy -> gets the data from a Nagios NDO MySQL Database (fast an reliable
 ;          but NDO is needed)
 backend="ndomy"
 ; select language (english,german,...)
 language="english"
 ; default icons
-defaultIcons="std_medium"
+defaulticons="std_medium"
 ; rotate maps (0/1)
-rotateMaps=0
+rotatemaps=0
 ; maps to rotate
-maps=demo,demo2
+maps="demo,demo2"
 ; show header (0/1)
-displayHeader=1
+displayheader=1
 ; options per line in header
-headerCount=3
-; config check on startup
-checkConfig=0
+headercount=3
 ; use gdlibs (if set to 0 lines will not work, all other types should work fine)
-useGDLibs=1
+usegdlibs=1
 ; refresh time of pages
-refreshTime=60
+refreshtime=60
 
 ; options for the wui
 [wui]
 ; auto update fequency
-autoUpdateFreq=25
+autoupdatefreq=25
 
 
 ; path options
@@ -51,39 +49,39 @@ icon="/usr/local/nagios/share/nagvis/iconsets/"
 ; absolute physical NagVis maps path
 map="/usr/local/nagios/share/nagvis/maps/"
 ; absolute physical NagVis maps cfg path
-mapCfg="/usr/local/nagios/share/nagvis/etc/maps/"
+mapcfg="/usr/local/nagios/share/nagvis/etc/maps/"
 ; absolute html NagVis path
-htmlBase="/nagios/nagvis"
+htmlbase="/nagios/nagvis"
 ; absolute html NagVis cgi path
-htmlCgi="/nagios/cgi-bin"
+htmlcgi="/nagios/cgi-bin"
 ; absolute html NagVis icon path
-htmlIcon="/nagios/nagvis/iconsets/"
+htmlicon="/nagios/nagvis/iconsets/"
 ; absolute html NagVis maps path
-htmlMap="/nagios/nagvis/maps/"
+htmlmap="/nagios/nagvis/maps/"
 ; absolute html NagVis documentation path
-htmlDoku="http://luebben-home.de/nagvis-doku/nav.html?nagvis/"
+htmldoku="http://luebben-home.de/nagvis-doku/nav.html?nagvis/"
 
 ; options for the NDO-Backend
 [backend_ndo]
 ; hostname for NDO-db
-dbHost=localhost
+dbhost="localhost"
 ; portname for NDO-db
-dbPort=3306
+dbport=3306
 ; database-name for NDO-db
-dbName=nagios
+dbname="nagios"
 ; username for NDO-db
-dbUser=nagios
+dbuser="nagios"
 ; password for NDO-db
-dbPass=password
+dbpass="nagios"
 ; prefix for tables in NDO-db
-dbPrefix=ndo_
+dbprefix="ndo_"
 ; instace-id for tables in NDO-db
-dbInstanceId=1
+dbinstanceid=1
 
 ; options for the HTML-Backend
 [backend_html]
 ; with this user, the script reads the CGIs
-cgiUser="nagiosadmin"
+cgiuser="nagiosadmin"
 ; physical path to the CGIs
 cgi="/usr/local/nagios/sbin/"
 
@@ -100,19 +98,19 @@ index="index.nagvis.inc"
 ; debug on/off (0/1)
 debug=0
 ; debugStates on/off (0/1)
-debugStates=0
+debugstates=0
 ; debugCheckState on/off (0/1)
-debugCheckState=0
+debugcheckstate=0
 ; debugFixIcon on/off (0/1)
-debugFixIcon=0
+debugfixicon=0
 
 ; Only internal informations
 [internal]
 ; version of NagVis
-version="0.9b3"
+version="0.9b3+"
 ; title of NagVis pages
-title="NagVis 0.9b3"
+title="NagVis 0.9b3+ (CVS Snapshot)"
 
 ; -------------------------
 ; EOF
-; -------------------------
+; ------------------------
