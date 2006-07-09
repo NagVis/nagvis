5e3682056ef580209469b74022437ef5f1b82a43
downanup <downanup@ff461055-cf1a-0410-99e4-fa365203d4c3>
2006-07-09 11:35:30 +0000

    Better Error Message
    git-svn-id: file:///root/orig/nagvis/trunk@323 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/class.CheckState_ndomy.php
+++ nagvis/includes/classes/class.CheckState_ndomy.php
@@ -88,7 +88,7 @@ class backend
 		//Be suspiciosly and check that the data at the db are not older that "maxTimeWithoutUpdate"  too
 		if(time() - strtotime($nagiosState['status_update_time']) > $maxTimeWithOutUpdate) {
 			//FIXME:Error Box
-			echo "<h1>NDOMy Backend: Caution: NDO claims that Nagios did NO status Update for more than ".$maxTimeWithOutUpdate." seconds! Make sure that Nagios is running!</h1>";
+			echo "<h1>NDOMy Backend: Caution: NDO claims that Nagios did NO status Update for more than ".$maxTimeWithOutUpdate." seconds! Make sure that Nagios and ndo2db are running!</h1>";
 			exit(1);
 		}
 			
