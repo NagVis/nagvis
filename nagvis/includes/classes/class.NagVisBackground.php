b531c0711241e74604d5f829ae5da99c65b4e437
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2006-11-01 11:29:54 +0000

    error messages in background are now read from the language files
    git-svn-id: file:///root/orig/nagvis/trunk@428 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/class.NagVisBackground.php
+++ nagvis/includes/classes/class.NagVisBackground.php
@@ -66,26 +66,13 @@ class NagVisBackground extends GlobalMap {
 		$this->GRAPHIC->init($this->image);
 	}
 	
-	function getColor($state){
-		if($state == 'OK' || $state == 'UP') {
-			$color = $this->intOk;
-		} elseif($state == 'WARNING') {
-			$color = $this->intWarning;
-		} elseif($state == 'CRITICAL' || $state == 'DOWN') {
-			$color = $this->intCritical;
-		} else {
-			$color = $this->intUnknown;
-		}
-		
-		return $color;
-	}
 	
 	function checkPreflight() {
 		if(!$this->MAPCFG->checkMapImageExists(0)) {
-			errorBox('The defined image doesn\'t exists!');
+			errorBox($this->LANG->getMessageText('backgroundNotExists','IMGPATH~'.$this->MAINCFG->getValue('paths', 'map').$this->MAPCFG->getImage()));
 		}
 		if(!$this->MAPCFG->checkMapImageReadable(0)) {
-			errorBox('The defined image isn\'t readable!');
+			errorBox($this->LANG->getMessageText('backgroundNotReadable','IMGPATH~'.$this->MAINCFG->getValue('paths', 'map').$this->MAPCFG->getImage()));
 		}
 		// FIXME: Check permissions?
 	}
@@ -125,6 +112,12 @@ class NagVisBackground extends GlobalMap {
 		}
 	}
 	
+	/**
+	 * Prints out an error box
+	 *
+	 * @param	String	$msg	String with error message
+	 * @author 	Lars Michelsen <larsi@nagios-wiki.de>
+	 */
 	function errorBox($msg) {
 		$this->image = @imagecreate(600,50);
 		$this->imageType = 'png';
@@ -187,6 +180,13 @@ class NagVisBackground extends GlobalMap {
 		return $ret;
 	}
 	
+	/**
+	 * Parses a line on the image
+	 *
+	 * @param	Array	$obj
+	 * @return	Array 	Array with Html Code
+	 * @author 	Lars Michelsen <larsi@nagios-wiki.de>
+	 */
 	function parseLine($obj) {
 		if($obj['type'] == 'service') {
 			$name = 'host_name';
@@ -228,228 +228,23 @@ class NagVisBackground extends GlobalMap {
 	}
 	
 	/**
-	 * Create a Link for a Line
+	 * Gets the color to print for the different states
 	 *
-	 * @param	Array	$obj	Array with object informations
-	 * @param	String	$name	Key for Objectname eg. host_name
-	 * @return	Array	Array with HTML Code
-	 * @fixme 	FIXME: optimize
-	 */
-	function createBoxLine($obj,$name) {
-		$ret = Array();
-	    if($obj['line_type'] == '10' || $obj['line_type'] == '11'){
-			list($x_from,$x_to) = explode(",", $obj['x']);
-			list($y_from,$y_to) = explode(",", $obj['y']);
-			$obj['x'] = middle($x_from,$x_to);
-			$obj['y'] = middle($y_from,$y_to);
-			$obj['icon'] = '20x20.gif';
-			
-			$obj = $this->fixIconPosition($obj);
-			$ret = array_merge($ret,$this->parseIcon($obj));
-		} elseif($mapCfg['line_type'] == '20') {
-			list($host_name_from,$host_name_to) = explode(",", $mapCfg[$name]);
-			list($service_description_from,$service_description_to) = explode(",", $mapCfg['service_description']);
-			list($x_from,$x_to) = explode(",", $mapCfg['x']);
-			list($y_from,$y_to) = explode(",", $mapCfg['y']);
-			
-			// From
-			$obj['x'] = middle2($x_from,$x_to);
-			$obj['y'] = middle2($y_from,$y_to);
-			$obj['icon'] = '20x20.gif';
-			
-			$obj = $this->fixIconPosition($obj);
-			$ret = array_merge($ret,$this->parseIcon($obj));
-			
-			// To
-			$obj['x'] = middle2($x_to,$x_from);
-			$obj['y'] = middle2($y_to,$y_from);
-			$obj = $this->fixIconPosition($obj);
-			$ret = array_merge($ret,$this->parseIcon($obj));
-		}
-		return $ret;
-	}
-	
-	/**
-	 * Parses the HTML-Code of an icon
-	 *
-	 * @param	Array	$obj	Array with object informations
-	 * @return	Array	Array with Html Code
-	 * @author	Lars Michelsen <larsi@nagios-wiki.de>
-	 */
-	function parseIcon($obj) {
-		$ret = Array();
-		
-		if($obj['type'] == 'service') {
-			$name = 'host_name';
-		} else {
-			$name = $obj['type'] . '_name';
-		}
-		
-		$ret[] = "<div class=\"icon\" style=\"left:".$obj['x']."px; top:".$obj['y']."px;\">";
-		$ret[] = "\t".$this->createLink($obj);
-		$ret[] = "\t\t<img src=\"".$this->MAINCFG->getValue('paths', 'htmlicon').$obj['icon']."\" ".$this->getHoverMenu($obj).";>";
-		$ret[] = "\t</a>";
-		$ret[] = "</div>";
-		
-		return $ret;
-	}
-	
-	/**
-	 * Creates a link to Nagios, when this is not set in the Config-File
-	 *
-	 * @param	Array	$obj	Array with object informations
-	 * @return	String	The Link
-	 * @author 	Michael Luebben <michael_luebben@web.de>
+	 * @param	String	$state
+	 * @return	Integer		Color to use
 	 * @author 	Lars Michelsen <larsi@nagios-wiki.de>
 	 */
-	function createLink($obj) {
-		if($obj['type'] == 'service') {
-			$name = 'host_name';
+	function getColor($state){
+		if($state == 'OK' || $state == 'UP') {
+			$color = $this->intOk;
+		} elseif($state == 'WARNING') {
+			$color = $this->intWarning;
+		} elseif($state == 'CRITICAL' || $state == 'DOWN') {
+			$color = $this->intCritical;
 		} else {
-			$name = $obj['type'] . '_name';
+			$color = $this->intUnknown;
 		}
 		
-		if(isset($obj['url'])) {
-			$link = '<A HREF='.$obj['url'].'>';
-    	} elseif($obj['type'] == 'map') {
-			$link = '<A HREF="'.$this->MAINCFG->getValue('paths', 'htmlbase').'/index.php?map='.$obj[$name].'">';
-    	}  elseif($obj['type'] == 'host') {
-			$link = '<A HREF="'.$this->MAINCFG->getValue('paths', 'htmlcgi').'/status.cgi?host='.$obj[$name].'">';
-    	} elseif($obj['type'] == 'service') {
-			$link = '<A HREF="'.$this->MAINCFG->getValue('paths', 'htmlcgi').'/extinfo.cgi?type=2&host='.$obj[$name].'&service='.$obj['service_description'].'">';
-    	} elseif($obj['type'] == 'hostgroup') {
-			$link = '<A HREF="'.$this->MAINCFG->getValue('paths', 'htmlcgi').'/status.cgi?hostgroup='.$obj[$name].'&style=detail">';
-    	} elseif($obj['type'] == 'servicegroup') {
-			$link = '<A HREF="'.$this->MAINCFG->getValue('paths', 'htmlcgi').'/status.cgi?servicegroup='.$obj[$name].'&style=detail">';
-    	}
-    	return $link;
-	}
-	
-	/**
-	 * Create a Comment-Textbox
-	 *
-	 * @param	Array	$obj	Array with object informations
-	 * @return	String	Array with HTML Code
-	 * @author	Joerg Linge
-	 * @author	Lars Michelsen <larsi@nagios-wiki.de>
-	 */
-	function textBox($obj) {
-		$ret = Array();
-		$ret[] = "<div class=\"".$obj['class']."\" style=\"background:".$obj['background_color'].";left: ".$obj['x']."px; top: ".$obj['y']."px; width: ".$obj['w']."px; overflow: visible;\">";	
-		$ret[] = "\t<span>".$obj['text']."</span>";
-		$ret[] = "</div>";
-		return $ret;	
-	}
-	
-	/**
-	 * Creates a hover box for objects
-	 *
-	 * @param	Array	$obj	Array with object informations
-	 * @return	String	Code for the hover box
-	 * @author	Lars Michelsen <larsi@nagios-wiki.de>
-     */
-	function getHoverMenu($obj) {
-		$ret = '';
-		// FIXME: check if this is an object, where a menu should be displayed
-		if(1) {
-			$ret .= 'onmouseover="return overlib(\'';
-			if($obj['hover_url']) {
-				$ret .= $this->readHoverUrl($obj);
-			} else {
-				$ret .= $this->createInfoBox($obj);
-			}
-			$ret .= '\', CAPTION, \''.$this->LANG->getLabel($obj['type']).'\', SHADOW, WRAP, VAUTO);" onmouseout="return nd();" ';
-			
-			return $ret;
-		}
-	}
-	
-	/**
-	 * Reads the given hover url form an object and forms it to a readable format for the hover box
-	 *
-	 * @param	Array	$obj	Array with object informations
-	 * @return	String	Code for the hover box
-	 * @author	Lars Michelsen <larsi@nagios-wiki.de>
-     */
-	function readHoverUrl($obj) {
-		/* FIXME: Context is supported in php >= 5.0
-		* $http_opts = array(
-		*      'http'=>array(
-		*      'method'=>"GET",
-		*      'header'=>"Accept-language: en\r\n" .
-		*                "Authorization: Basic ".base64_encode("user:pw"),
-		*      'request_fulluri'=>true  ,
-		*      'proxy'=>"tcp://proxy.url.de"
-		*   )
-		* );
-		* $context = stream_context_create($http_opts);
-		* $content = file_get_contents($obj['hover_url'],FALSE,$context);
-		*/
-		
-		if(!$content = @file_get_contents($obj['hover_url'])) {
-			$FRONTEND = new GlobalPage($this->MAINCFG,Array('languageRoot'=>'nagvis:global'));
-	        $FRONTEND->messageToUser('WARNING','couldNotGetHoverUrl','URL~'.$obj['hover_url']);
-		}
-		return str_replace('"','\\\'',str_replace('\'','\\\'',$content));
-	}
-	
-	/**
-	 * Creates a Java-Box with information.
-	 *
-	 * @param	Array	$obj	Array with object informations
-	 * @return	String	Code for the Hover-Box
-	 * @author	Michael Luebben <michael_luebben@web.de>
-	 * @author	Lars Michelsen <larsi@nagios-wiki.de>
-	 * @fixme	FIXME: optimize
-     */
-	function createInfoBox($obj) {
-		if($obj['type'] == 'service') {
-			$name = 'host_name';
-		} else {
-			$name = $obj['type'] . '_name';
-		}
-		
-		if(!isset($obj['stateCount'])) {
-			$obj['stateCount'] = 0;
-		}
-		
-		$Count = $obj['stateCount'];
-		$obj['stateCount'] = str_replace('"',"",$obj['stateCount']);
-		$obj['stateCount'] = str_replace("'","",$obj['stateCount']);
-		$ServiceHostState = $obj['stateHost'];
-		
-		// FIXME mehr Output (ackComment, mehr Zahlen etc.)
-		switch($obj['type']) {
-			case 'host':
-				$info .= '<b>'.$this->LANG->getLabel('hostname').':</b> '.$obj[$name].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('state').':</b> '.$obj['state'].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('output').':</b> '.strtr(addslashes($obj['stateOutput']), array("\r" => '<br>', "\n" => '<br>')).'<br>'; 
-			break;
-			case 'service':
-				$info .= '<b>'.$this->LANG->getLabel('hostname').':</b> '.$obj[$name].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('servicename').':</b> '.$obj['service_description'].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('state').':</b> '.$obj['state'].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('output').':</b> '.strtr(addslashes($obj['stateOutput']), array("\r" => '<br>', "\n" => '<br>')).'<br>';
-			break;
-			case 'hostgroup':
-				$info .= '<b>'.$this->LANG->getLabel('hostgroupname').':</b> '.$obj[$name].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('state').':</b> '.$obj['state'].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('output').':</b> '.strtr(addslashes($obj['stateOutput']), array("\r" => '<br>', "\n" => '<br>')).'<br>'; 
-			break;
-			case 'servicegroup':
-				$info .= '<b>'.$this->LANG->getLabel('servicegroupname').':</b> '.$obj[$name].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('state').':</b> '.$obj['state'].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('output').':</b> '.strtr(addslashes($obj['stateOutput']), array("\r" => '<br>', "\n" => '<br>')).'<br>'; 
-			break;
-			case 'map':
-				$info .= '<b>'.$this->LANG->getLabel('mapname').':</b> '.$obj[$name].'<br>';
-				$info .= '<b>'.$this->LANG->getLabel('state').':</b> '.strtr(addslashes($obj['state']), array("\r" => '<br>', "\n" => '<br>')).'<br>'; 
-				$info .= '<b>'.$this->LANG->getLabel('output').':</b> '.strtr(addslashes($obj['stateOutput']), array("\r" => '<br>', "\n" => '<br>')).'<br>'; 
-			break;
-			default:
-				//FIXME Error
-			break;
-		}
-		return $info;
+		return $color;
 	}
 }
