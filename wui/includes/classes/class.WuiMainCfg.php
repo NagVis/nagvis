6433ec14acaa5470cce260f747dbd742e3327fa3
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2006-12-11 10:42:19 +0000

    Added WuiMainCfg.php
    git-svn-id: file:///root/orig/nagvis/trunk@470 ff461055-cf1a-0410-99e4-fa365203d4c3

---
+++ wui/includes/classes/class.WuiMainCfg.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This Class handles the NagVis configuration file for the wui
+ */
+class WuiMainCfg extends GlobalMainCfg {
+	
+	/**
+	 * Class Constructor
+	 *
+	 * @param	String	$configFile			String with path to config file
+	 * @author Lars Michelsen <larsi@nagios-wiki.de>
+	 */
+	function WuiMainCfg($configFile) {
+		parent::GlobalMainCfg($configFile);
+	}
+	
+	function getMaps() {
+		$files = Array();
+		
+		if ($handle = opendir($this->getValue('paths', 'mapcfg'))) {
+ 			while (false !== ($file = readdir($handle))) {
+				if ($file != "." && $file != ".." && substr($file,strlen($file)-4,4) == ".cfg") {
+					$files[] = substr($file,0,strlen($file)-4);
+				}				
+			}
+			
+			if ($files) {
+				natcasesort($files);
+			}
+		}
+		closedir($handle);
+		
+		return $files;
+	}
+	
+	/**
+	 * Sets a config section in the config array
+	 *
+	 * @param	String	$sec	Section
+	 * @return	Boolean	Is Successful?
+	 * @author 	Lars Michelsen <larsi@nagios-wiki.de>
+     */
+	function setSection($sec) {
+		$this->config[$sec] = Array();
+		
+		return TRUE;
+	}
+	
+	/**
+	 * Deletes a config section in the config array
+	 *
+	 * @param	String	$sec	Section
+	 * @return	Boolean	Is Successful?
+	 * @author 	Lars Michelsen <larsi@nagios-wiki.de>
+     */
+	function delSection($sec) {
+		$this->config[$sec] = '';
+		unset($this->config[$sec]);
+		
+		return TRUE;
+	}
+}
+?>
