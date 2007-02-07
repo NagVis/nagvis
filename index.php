022daae9b8224e6315f730a7fa100cdd3fcdf618
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-02-07 08:16:40 +0000

    config.php/index.php https/http redirection
    git-svn-id: file:///root/orig/nagvis/trunk@533 ff461055-cf1a-0410-99e4-fa365203d4c3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: http://" . $_SERVER['HTTP_HOST'] 
+header("Location: ". ((isset($_SERVER["HTTPS"])) ? 'https://': 'http://') . $_SERVER['HTTP_HOST'] 
 	. rtrim(dirname($_SERVER['PHP_SELF']), '/\\')
     . "/nagvis/index.php".(($_SERVER["QUERY_STRING"] != '') ? '?':'').$_SERVER["QUERY_STRING"]);
 ?>
