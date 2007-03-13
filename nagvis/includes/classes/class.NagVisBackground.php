e628b12bcb136ce2f0d0aef2eb82def05784168d
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-03-13 19:25:34 +0000

    modified sth in NagVisBackground memoryGetUsage method
    git-svn-id: file:///root/orig/nagvis/trunk@538 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/class.NagVisBackground.php
+++ nagvis/includes/classes/class.NagVisBackground.php
@@ -83,23 +83,28 @@ class NagVisBackground extends GlobalMap {
 	 * @author 	Lars Michelsen <larsi@nagios-wiki.de>
 	 */
 	function memoryGetUsage() {
-		//If its Windows
-		//Tested on Win XP Pro SP2. Should work on Win 2003 Server too
-		//Doesn't work for 2000
-		//If you need it to work for 2000 look at http://us2.php.net/manual/en/function.memory-get-usage.php#54642
+		// If function already exists in PHP, use it!
+		if(function_exists('memory_get_usage')) {
+			return memory_get_usage();
+		}
+
+		// If its Windows
+		// Tested on Win XP Pro SP2. Should work on Win 2003 Server too
+		// If you need it to work for 2000 look at http://us2.php.net/manual/en/function.memory-get-usage.php#54642
 		if(substr(PHP_OS,0,3) == 'WIN') {
 			$output = array();
 			exec( 'tasklist /FI "PID eq ' . getmypid() . '" /FO LIST', $output );
 			
 			return preg_replace( '/[\D]/', '', $output[5] ) * 1024;
 		} else {
-			//We now assume the OS is UNIX
-			//Tested on Mac OS X 10.4.6 and Linux Red Hat Enterprise 4
-			//This should work on most UNIX systems
+			// We now assume the OS is UNIX
+			// Tested on Mac OS X 10.4.6 and Linux Red Hat Enterprise 4
+			// This should work on most UNIX systems
 			$pid = getmypid();
 			exec("ps -eo%mem,rss,pid | grep $pid", $output);
 			$output = explode("  ", $output[0]);
-			//rss is given in 1024 byte units
+			
+			// rss is given in 1024 byte units
 			return $output[1] * 1024;
 		}
 	} 
