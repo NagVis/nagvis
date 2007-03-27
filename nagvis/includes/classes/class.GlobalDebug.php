a1b8def9012d80d8f5760055b1bd43743a76dd07
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-03-27 19:27:41 +0000

    Extended Debug feature
    git-svn-id: file:///root/orig/nagvis/trunk@549 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/class.GlobalDebug.php
+++ nagvis/includes/classes/class.GlobalDebug.php
@@ -1,12 +1,24 @@
 <?php
 define('DEBUG',FALSE);
+/**
+ * For wanted debug output summarize these possible options:
+ * 1: function beginning and ending
+ * 2: progres informations in the functions
+ */
+define('DEBUGLEVEL',3);
 define('DEBUGFILE','/tmp/nagvis.debug');
 define('DEBUGSTART',microtime_float());
 
 function debug($msg) {
-	$fh=fopen(DEBUGFILE,'a');
-	fwrite($fh,utf8_encode(microtime_float().' '.$msg."\n"));
-	fclose($fh);
+		$fh=fopen(DEBUGFILE,'a');
+		fwrite($fh,utf8_encode(microtime_float().' '.$msg."\n"));
+		fclose($fh);
+}
+
+function debugFinalize() {
+	debug('###########################################################');
+	debug('Render Time: '.(microtime_float()-DEBUGSTART));
+	debug('###########################################################');
 }
 
 function microtime_float() {
