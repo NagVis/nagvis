a01b3144861d106bb55b98a6ca8817d54beb1403
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-05-07 18:24:22 +0000

    Moved Background forms from map_management to new page background_management
    git-svn-id: file:///root/orig/nagvis/trunk@612 ff461055-cf1a-0410-99e4-fa365203d4c3

---
+++ wui/includes/classes/class.WuiBackgroundManagement.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Class for managing the backgrounds in WUI
+ *
+ * @author 	Lars Michelsen <lars@vertical-visions.de>
+ */
+class WuiBackgroundManagement extends GlobalPage {
+	var $MAINCFG;
+	var $LANG;
+	var $ADDFORM;
+	var $DELFORM;
+	var $propCount;
+	
+	/**
+	 * Class Constructor
+	 *
+	 * @param	GlobalMainCfg	$MAINCFG
+	 * @author	Lars Michelsen <lars@vertical-visions.de>
+	 */
+	function WuiBackgroundManagement(&$MAINCFG) {
+		$this->MAINCFG = &$MAINCFG;
+		$this->propCount = 0;
+		
+		// load the language file
+		$this->LANG = new GlobalLanguage($MAINCFG,'wui:backgroundManagement');
+		
+		$prop = Array('title'=>$MAINCFG->getValue('internal', 'title'),
+					  'cssIncludes'=>Array('./includes/css/wui.css'),
+					  'jsIncludes'=>Array('./includes/js/BackgroundManagement.js',
+					  						'./includes/js/ajax.js',
+					  						'./includes/js/wui.js'),
+					  'extHeader'=>Array(''),
+					  'allowedUsers' => Array('EVERYONE'),
+					  'languageRoot' => 'wui:backgroundManagement');
+		parent::GlobalPage($MAINCFG,$prop);
+	}
+	
+	/**
+	* If enabled, the form is added to the page
+	*
+	* @author Lars Michelsen <lars@vertical-visions.de>
+	*/
+	function getForm() {
+		// Inititalize language for JS
+		$this->addBodyLines($this->getJsLang());
+		
+		$this->ADDFORM = new GlobalForm(Array('name'=>'new_image',
+			'id'=>'new_image',
+			'method'=>'POST',
+			'action'=>'./wui.function.inc.php?myaction=mgt_new_image',
+			'onSubmit'=>'return check_image_add();',
+			'enctype'=>'multipart/form-data',
+			'cols'=>'2'));
+		$this->addBodyLines($this->ADDFORM->initForm());
+		$this->addBodyLines($this->ADDFORM->getCatLine(strtoupper($this->LANG->getLabel('uploadBackground'))));
+		$this->propCount++;
+		$this->addBodyLines($this->getAddFields());
+		$this->addBodyLines($this->getAddSubmit());
+		
+		$this->DELFORM = new GlobalForm(Array('name'=>'image_delete',
+			'id'=>'image_delete',
+			'method'=>'POST',
+			'action'=>'./wui.function.inc.php?myaction=mgt_image_delete',
+			'onSubmit'=>'return check_image_delete();',
+			'cols'=>'2'));
+		$this->addBodyLines($this->DELFORM->initForm());
+		$this->addBodyLines($this->DELFORM->getCatLine(strtoupper($this->LANG->getLabel('deleteBackground'))));
+		$this->propCount++;
+		$this->addBodyLines($this->getDelFields());
+		$this->addBodyLines($this->getDelSubmit());
+		
+		// Resize the window
+		$this->addBodyLines($this->resizeWindow());
+	}
+	
+	/**
+	 * Resizes the window to individual calculated size
+	 *
+	 * @return	Array	HTML Code
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+     */
+	function resizeWindow() {
+		$ret = Array();
+		$ret[] = "<script type=\"text/javascript\" language=\"JavaScript\"><!--";
+		$ret[] = "// resize the window (depending on the number of properties displayed)";
+		$ret[] = "window.resizeTo(window.innerWidth,".$this->propCount."*40+10)";
+		$ret[] = "//--></script>";
+		
+		return $ret;
+	}
+	
+	/**
+	 * Gets delete fields
+	 *
+	 * @return	Array	HTML Code
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+     */
+	function getDelFields() {
+		$ret = Array();
+		$ret = array_merge($ret,$this->DELFORM->getSelectLine($this->LANG->getLabel('choosePngImage'),'map_image',$this->getMapImages(),''));
+		$this->propCount++;
+		
+		return $ret;
+	}
+	
+	/**
+	 * Gets delete submit button
+	 *
+	 * @return	Array	HTML Code
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+     */
+	function getDelSubmit() {
+		$this->propCount++;
+		return array_merge($this->DELFORM->getSubmitLine($this->LANG->getLabel('delete')),$this->DELFORM->closeForm());
+	}
+	
+	/**
+	 * Gets new image fields
+	 *
+	 * @return	Array	HTML Code
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+     */
+	function getAddFields() {
+		$ret = Array();
+		$ret = array_merge($ret,$this->ADDFORM->getHiddenField('MAX_FILE_SIZE','1000000'));
+		$ret = array_merge($ret,$this->ADDFORM->getFileLine($this->LANG->getLabel('choosePngImage'),'image_file',''));
+		$this->propCount++;
+		
+		return $ret;
+	}
+	
+	/**
+	 * Gets new image submit button
+	 *
+	 * @return	Array	HTML Code
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+     */
+	function getAddSubmit() {
+		$this->propCount++;
+		return array_merge($this->ADDFORM->getSubmitLine($this->LANG->getLabel('upload')),$this->ADDFORM->closeForm());
+	}
+	
+	/**
+	 * Reads all map images in map path
+	 *
+	 * @return	Array map images
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+	 */
+	function getMapImages() {
+		$files = Array();
+		
+		if ($handle = opendir($this->MAINCFG->getValue('paths', 'map'))) {
+ 			while (false !== ($file = readdir($handle))) {
+				if ($file != "." && $file != ".." && substr($file,strlen($file)-4,4) == ".png") {
+					$files[] = $file;
+				}				
+			}
+			
+			if ($files) {
+				natcasesort($files);
+			}
+		}
+		closedir($handle);
+		
+		return $files;
+	}
+	
+	/**
+	 * Gets all needed messages
+	 *
+	 * @return	Array Html
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+	 */
+	function getJsLang() {
+		$ret = Array();
+		$ret[] = '<script type="text/javascript" language="JavaScript"><!--';
+		$ret[] = 'var lang = Array();';
+		$ret[] = 'lang["firstMustChoosePngImage"] = "'.$this->LANG->getMessageText('firstMustChoosePngImage').'";';
+		$ret[] = 'lang["mustChoosePngImage"] = "'.$this->LANG->getMessageText('mustChoosePngImage').'";';
+		$ret[] = 'lang["foundNoBackgroundToDelete"] = "'.$this->LANG->getMessageText('foundNoBackgroundToDelete').'";';
+		$ret[] = 'lang["confirmBackgroundDeletion"] = "'.$this->LANG->getMessageText('confirmBackgroundDeletion').'";';
+		$ret[] = 'lang["unableToDeleteBackground"] = "'.$this->LANG->getMessageText('unableToDeleteBackground').'";';
+		$ret[] = '//--></script>';
+		
+		return $ret;	
+	}
+}
+?>
---
+++ wui/includes/js/BackgroundManagement.js
@@ -0,0 +1,54 @@
+// checks that the file the user wants to upload has the .png extension
+function checkPng(imageName) {
+	if(imageName.substring(imageName.length-3,imageName.length).toLowerCase() != 'png') {
+		return false; 
+	} else {
+		return true;
+	}
+}
+
+function checkImageUsed(imageName,mapOptions) {
+	for(var i=0;i<mapOptions.length;i++) {
+		if(mapOptions[i].mapImage == imageName) {
+			return mapOptions[i].mapName;
+		}
+	}
+	
+	return "";
+}
+
+function check_image_add() {
+	if(document.new_image.image_file.value.length == 0) {
+		alert(printLang(lang['firstMustChoosePngImage'],''));
+		
+		return false;
+	}
+	
+	if(!checkPng(document.new_image.image_file.value)) {
+		alert(printLang(lang['mustChoosePngImage'],''));
+		
+		return false;
+	}
+	
+	return true;
+	
+}
+
+function check_image_delete() {
+	if(document.image_delete.map_image.value == '') {
+		alert(printLang(lang['foundNoBackgroundToDelete'],''));
+		return false;
+	}
+	
+	imageUsedBy = checkImageUsed(document.image_delete.map_image.value, window.opener.mapOptions);
+	if(imageUsedBy != "") {
+		alert(printLang(lang['unableToDeleteBackground'],'MAP~'+imageUsedBy+',IMAGENAME~'+document.image_delete.map_image.value));
+		return false;
+	}
+	
+	if(confirm(printLang(lang['confirmBackgroundDeletion'],'')) === false) {
+		return false;
+	}
+	
+	return true;
+}
