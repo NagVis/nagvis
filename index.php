bbc92f8034285614cef6cc1fa4322dbb63bd217b
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-05-16 17:45:58 +0000

    redirection files in nagvis root no recognize given ports
    git-svn-id: file:///root/orig/nagvis/trunk@652 ff461055-cf1a-0410-99e4-fa365203d4c3

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 header("Location: ". ((isset($_SERVER["HTTPS"])) ? 'https://': 'http://') . $_SERVER['HTTP_HOST'] 
+	. ((isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != '80') ? ':'.$_SERVER['SERVER_PORT']: '')
 	. rtrim(dirname($_SERVER['PHP_SELF']), '/\\')
     . "/nagvis/index.php".(($_SERVER["QUERY_STRING"] != '') ? '?':'').$_SERVER["QUERY_STRING"]);
 ?>
