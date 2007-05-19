865973b18eec168c834ae2f6e52ade8d8e8f050d
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-05-19 16:32:15 +0000

    Fixed problem with PHP 4.3 bug in html_entity_decode when decoding UTF-8
    git-svn-id: file:///root/orig/nagvis/trunk@660 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/class.GlobalLanguage.php
+++ nagvis/includes/classes/class.GlobalLanguage.php
@@ -138,9 +138,52 @@ class GlobalLanguage {
 	 */
 	function replaceSpecial($str) {
 		if (DEBUG&&DEBUGLEVEL&1) debug('Start method GlobalLanguage::replaceSpecial(String)');
-		$str = html_entity_decode($str,ENT_NOQUOTES,'UTF-8');
+		// DEPRECATED: This is buggy in PHP 4.3, using own methods
+		//$str = html_entity_decode($str,ENT_NOQUOTES,'UTF-8');
+		$str = $this->htmlEntityDecodeUtf8($str);
 		if (DEBUG&&DEBUGLEVEL&1) debug('End method GlobalLanguage::replaceSpecial(): String');
 		return $str;
+	}
+	
+	/**
+	 * Returns the utf string corresponding to the unicode value (from php.net)
+	 *
+	 * @param	String	raw string
+	 * @return	String	UTF8 encoded string
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+     */
+	function htmlEntityDecodeUtf8($string) {
+	    static $trans_tbl;
+	   
+	    // replace numeric entities
+	    $string = preg_replace('~&#x([0-9a-f]+);~ei', 'code2utf(hexdec("\\1"))', $string);
+	    $string = preg_replace('~&#([0-9]+);~e', 'code2utf(\\1)', $string);
+	
+	    // replace literal entities
+	    if (!isset($trans_tbl)) {
+	        $trans_tbl = array();
+	       
+	        foreach (get_html_translation_table(HTML_ENTITIES) as $val=>$key) {
+	            $trans_tbl[$key] = utf8_encode($val);
+	        }
+	    }
+	   
+	    return strtr($string, $trans_tbl);
+	}
+	
+	/**
+	 * Returns the utf string corresponding to the unicode value (from php.net)
+	 *
+	 * @param	Integer	Unicode value
+	 * @return	String 	UTF string
+	 * @author 	Lars Michelsen <lars@vertical-visions.de>
+     */
+	function code2utf($num) {
+	    if ($num < 128) return chr($num);
+	    if ($num < 2048) return chr(($num >> 6) + 192) . chr(($num & 63) + 128);
+	    if ($num < 65536) return chr(($num >> 12) + 224) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);
+	    if ($num < 2097152) return chr(($num >> 18) + 240) . chr((($num >> 12) & 63) + 128) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);
+	    return '';
 	}
 	
 	/**
