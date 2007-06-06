8c9a489a5f390c93ed1aad045f275c38ebeffca7
MiCkEy2002 <MiCkEy2002@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-06-06 14:22:13 +0000

    Change macro [html_images] to [tmpl_images]
    git-svn-id: file:///root/orig/nagvis/trunk@688 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/class.NagVisFrontend.php
+++ nagvis/includes/classes/class.NagVisFrontend.php
@@ -85,7 +85,7 @@ class NagVisFrontend extends GlobalPage {
 				// Replace some macros
 				$ret = str_replace('[current_map]',$this->MAPCFG->getName(),$ret);
 				$ret = str_replace('[current_map_alias]',$this->MAPCFG->getValue('global', '0', 'alias'),$ret);
-				$ret = str_replace('[html_images]',$this->MAINCFG->getValue('paths','htmlimages'),$ret);
+				$ret = str_replace('[tmpl_images]',$this->MAINCFG->getValue('paths','htmlimages').'/templates',$ret);
 				$ret = str_replace('[html_base]',$this->MAINCFG->getValue('paths','htmlbase'),$ret);
 				// Replace language macros
 				$ret = str_replace('[lang_select_map]',$this->LANG->getLabel('selectMap'),$ret);
