f67db9099263e345769600e57bae593cdcce3f42
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-06-27 15:33:37 +0000

    removed deprecated code
    git-svn-id: file:///root/orig/nagvis/trunk@767 ff461055-cf1a-0410-99e4-fa365203d4c3

--- share/nagvis/includes/js/nagvis.js
+++ share/nagvis/includes/js/nagvis.js
@@ -1,19 +1,13 @@
-if(parseInt(navigator.appVersion.substring(0,1))>=3) {
- doton = new Image(13,14);
- doton.src = "images/internal/orangedot.gif";
- dotoff = new Image(13,14);
- dotoff.src = "images/internal/greendot.gif";
-}
-
-function switchdot(name,on) {
- if(parseInt(navigator.appVersion.substring(0,1))>=3){
-  image = eval("" + (on == 1 ? "doton.src" : "dotoff.src"));
-  document[name].src=image;
- }
-}
-  
-function printPos($offset_x,$offset_y) {
- var Pos = "x: " + (event.clientX-$offset_x) + " / y:" + (event.clientY-$offset_y);
- window.status = Pos;
- return true;
+/**
+ * Checks the current map rotation setting and changes it
+ *
+ * @return	Array maps
+ * @author Lars Michelsen <lars@vertical-visions.de>
+ */
+function toggleMapRotation() {
+	// FIXME: Code this
+	// Check if the map rotation is enabled
+	// <META http-equiv="refresh" CONTENT="60;">
+	// Enable it if it is disabled
+	// Disable it if it is enabled
 }
