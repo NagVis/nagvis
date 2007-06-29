f8a20c3b14d3ac0cbb321465ae399fd8e3abbff8
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-06-29 20:51:43 +0000

    renamed map management js
    git-svn-id: file:///root/orig/nagvis/trunk@783 ff461055-cf1a-0410-99e4-fa365203d4c3

--- share/wui/includes/classes/class.WuiMapManagement.php
+++ share/wui/includes/classes/class.WuiMapManagement.php
@@ -30,7 +30,7 @@ class WuiMapManagement extends GlobalPage {
 		
 		$prop = Array('title'=>$MAINCFG->getValue('internal', 'title'),
 					  'cssIncludes'=>Array('./includes/css/wui.css'),
-					  'jsIncludes'=>Array('./includes/js/map_management.js',
+					  'jsIncludes'=>Array('./includes/js/MapManagement.js',
 					  						'./includes/js/ajax.js',
 					  						'./includes/js/wui.js'),
 					  'extHeader'=>Array(''),
