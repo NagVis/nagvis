1811d5b45f5e8cbb1268a22d842f6353d7d6c781
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-07-03 15:35:57 +0000

    Updated version tag
    git-svn-id: file:///root/orig/nagvis/branches/1.1@793 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/class.GlobalMainCfg.php
+++ nagvis/includes/classes/class.GlobalMainCfg.php
@@ -220,12 +220,12 @@ class GlobalMainCfg {
 																					'type' => 'string')))),
 			'internal' => Array('version' => Array('must' => 1,
 												 'editable' => 0,
-												'default' => '1.1b2',
+												'default' => '1.1rc1',
 												'locked' => 1,
 												'type' => 'string'),
 							'title' => Array('must' => 1,
 												 'editable' => 0,
-												'default' => 'NagVis 1.1b2',
+												'default' => 'NagVis 1.1rc1',
 												'locked' => 1,
 												'type' => 'string')));
 		
