16b8e23d32e87d8b285df18bd01050e6488564f8
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-09-27 17:48:04 +0000

    Added define files
    git-svn-id: file:///root/orig/nagvis/trunk@885 ff461055-cf1a-0410-99e4-fa365203d4c3

---
+++ nagvis/includes/defines/global.php
@@ -0,0 +1,19 @@
+<?PHP
+/**
+ * Here are defined some constants for use in NagVis and WUI
+ *
+ * @author      Lars Michelsen <lars@vertical-visions.de>
+ */
+
+define('DEBUG',FALSE);
+/**
+ * For wanted debug output summarize these possible options:
+ * 1: function beginning and ending
+ * 2: progres informations in the functions
+ * 4: render time
+ */
+define('DEBUGLEVEL', 1);
+define('DEBUGFILE', '/var/log/nagvis-debug.log');
+define('CONST_VERSION', '1.2a1');
+define('CONST_MAINCFG', '../etc/nagvis-config.ini.php');
+?>
