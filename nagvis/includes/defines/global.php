0a1bdcfe3bb7faf105c2f144208cf409b6f648fd
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2007-10-20 14:49:13 +0000

    Cleaned debug mode
    git-svn-id: file:///root/orig/nagvis/trunk@947 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/defines/global.php
+++ nagvis/includes/defines/global.php
@@ -5,14 +5,14 @@
  * @author      Lars Michelsen <lars@vertical-visions.de>
  */
 
-define('DEBUG',TRUE);
+define('DEBUG',FALSE);
 /**
  * For wanted debug output summarize these possible options:
  * 1: function beginning and ending
  * 2: progres informations in the functions
  * 4: render time
  */
-define('DEBUGLEVEL', 7);
+define('DEBUGLEVEL', 4);
 define('DEBUGFILE', '/var/log/nagvis-debug.log');
 
 define('CONST_VERSION', '1.2b1');
