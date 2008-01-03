441eb149aa783b8f42b9b9a1f1024d79e2880117
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-01-03 07:53:02 +0000

    Added new match for empty strings
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1059 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/defines/matches.php
+++ nagvis/includes/defines/matches.php
@@ -11,6 +11,7 @@ define('MATCH_ALL', '/^.*$/i');
 define('MATCH_STRING', '/^[0-9a-z\s\:\[\]\(\)\_\.\,\-\?\!\#\=\/\\\]+$/i');
 define('MATCH_STRING_EMPTY', '/^[0-9a-z\s\:\[\]\(\)\_\.\,\-\?\!\#\=\/\\\]*$/i');
 define('MATCH_STRING_NO_SPACE', '/^[0-9a-z\:\[\]\(\)\_\.\,\-\?\!\#\@\=\/\\\]+$/i');
+define('MATCH_STRING_NO_SPACE_EMPTY', '/^[0-9a-z\:\[\]\(\)\_\.\,\-\?\!\#\@\=\/\\\]*$/i');
 define('MATCH_STRING_PATH', '/^[0-9a-z\s\_\.\-\/\\\]+$/i');
 define('MATCH_STRING_URL', '/^[0-9a-z\s\:\[\]\(\)\=\?\&\_\.\-\#\@\=\/\\\]+$/i');
 
