77b5f7a0c6d0d735c45878dd64ecb3f8f7988340
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-01-07 07:10:20 +0000

    Added some comments
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1091 ff461055-cf1a-0410-99e4-fa365203d4c3

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+// This file redirects the request to the WUI index file (wui/index.php)
 header("Location: ".((isset($_SERVER["HTTPS"])) ? 'https://': 'http://') . $_SERVER['HTTP_HOST'] 
 	. ((isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != '80') ? ':'.$_SERVER['SERVER_PORT']: '')
 	. rtrim(dirname($_SERVER['PHP_SELF']), '/\\')
