3fe6e78a309a3f7844ccf45cc0a8504b6dd32897
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-01-15 07:53:38 +0000

    Removed debug output
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1132 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/NagVisBackground.php
+++ nagvis/includes/classes/NagVisBackground.php
@@ -26,7 +26,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function NagVisBackground(&$MAINCFG,&$MAPCFG,&$LANG,&$BACKEND) {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::NagVisBackground($MAINCFG,$MAPCFG,$LANG,$BACKEND)');
 		$this->MAINCFG = &$MAINCFG;
 		$this->MAPCFG = &$MAPCFG;
 		$this->LANG = &$LANG;
@@ -42,7 +41,6 @@ class NagVisBackground extends NagVisMap {
 		$this->checkPreflight();
 		$this->initImage();
 		
-		if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::NagVisBackground()');
 	}
 	
 	
@@ -53,21 +51,16 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function getUser() {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method GlobalPage::getUser()');
 		if(isset($_SERVER['PHP_AUTH_USER'])) {
-			if (DEBUG&&DEBUGLEVEL&1) debug('End method GlobalPage::getUser(): '.$_SERVER['PHP_AUTH_USER']);
 			return $_SERVER['PHP_AUTH_USER'];
 		} elseif(isset($_SERVER['REMOTE_USER'])) {
-			if (DEBUG&&DEBUGLEVEL&1) debug('End method GlobalPage::getUser(): '.$_SERVER['REMOTE_USER']);
 			return $_SERVER['REMOTE_USER'];
 		}
 	}
 	
 	function checkMemoryLimit($tryToFix=TRUE) {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::checkMemoryLimit('.$tryToFix.')');
 		$fileSize = filesize($this->MAINCFG->getValue('paths', 'map').$this->MAPCFG->BACKGROUND->getFileName());
 		$memoryLimit = preg_replace('/[a-z]/i','',ini_get('memory_limit'))*1024*1024;
-		if (DEBUG&&DEBUGLEVEL&2) debug('Memory Limit: '.$memoryLimit);
 		
 		$imageSize = getimagesize($this->MAINCFG->getValue('paths', 'map').$this->MAPCFG->BACKGROUND->getFileName());
 		if (is_array($imageSize)) {
@@ -93,18 +86,13 @@ class NagVisBackground extends NagVisMap {
 				if($tryToFix) {
 					ini_set('memory_limit',round(($memoryUsage + $rawSize)*1.15 / 1024 /1024).'M');
 					$return = $this->checkMemoryLimit(FALSE);
-					if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::checkMemoryLimit(): '.$return);
 					return $return;
 				}
-				if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::checkMemoryLimit(): FALSE');
 				return FALSE;
 			} else {
-				if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::checkMemoryLimit(): TRUE');
 				return TRUE;
 			}
 		} else {
-			if (DEBUG&&DEBUGLEVEL&2) debug('Memory Limit is less or equal 0, aborting method and hope the Best...');
-			if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::checkMemoryLimit(): TRUE');
 			return TRUE;
 		}
 	}
@@ -117,7 +105,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function memoryGetUsage() {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::memoryGetUsage()');
 		$iReturn = 0;
 		
 		// If function already exists in PHP, use it!
@@ -128,7 +115,6 @@ class NagVisBackground extends NagVisMap {
 		} else {
 			$sLog .= 'not exists!';
 		}
-		if (DEBUG&&DEBUGLEVEL&2) debug($sLog);
 		
 		
 		if($iReturn <= 0) {
@@ -138,27 +124,22 @@ class NagVisBackground extends NagVisMap {
 			$sLog = 'Check if WIN/UNIX...';
 			if(substr(PHP_OS,0,3) == 'WIN') {
 				$sLog .= 'WIN (PHP_OS: '.PHP_OS.')';
-				if (DEBUG&&DEBUGLEVEL&2) debug($sLog);
 				
 				$output = array();
 				exec( 'tasklist /FI "PID eq ' . getmypid() . '" /FO LIST', $output );
-				if (DEBUG&&DEBUGLEVEL&2) debug('Exec: tasklist /FI "PID eq \' . '.getmypid().' . \'" /FO LIST: '.implode(',',$output));
 				
 				$iReturn = preg_replace( '/[\D]/', '', $output[5] ) * 1024;
 			} else {
 				$sLog .= 'UNIX (PHP_OS: '.PHP_OS.')';
-				if (DEBUG&&DEBUGLEVEL&2) debug($sLog);
 				// We now assume the OS is UNIX
 				// Tested on Mac OS X 10.4.6 and Linux Red Hat Enterprise 4
 				// This should work on most UNIX systems
 				$pid = getmypid();
-				if (DEBUG&&DEBUGLEVEL&2) debug('Process ID: '.$pid);
 	
 				if($pid == 0) {
 					$iReturn = 0;
 				} else {
 					exec('ps -eo%mem,rss,pid | grep '.$pid, $output);
-					if (DEBUG&&DEBUGLEVEL&2) debug('Exec: ps -eo%mem,rss,pid | grep '.$pid.': '.implode(',',$output));
 					$output = explode('  ', $output[0]);
 					
 					// rss is given in 1024 byte units
@@ -168,16 +149,13 @@ class NagVisBackground extends NagVisMap {
 		}
 		
 		if($iReturn < 0) {
-			if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::memoryGetUsage(): 0');
 			return 0;
 		} else {
-			if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::memoryGetUsage(): '.$iReturn);
 			return $iReturn;
 		}
 	}
 	
 	function initImage() {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::initImage()');
 		$fileName = $this->MAPCFG->BACKGROUND->getFileName();
 		$this->imageType = strtolower(substr($fileName, strrpos($fileName, '.') + 1));
 		
@@ -200,7 +178,6 @@ class NagVisBackground extends NagVisMap {
 		$this->intUnknown = imagecolorallocate($this->image, 255, 128, 0);
 		
 		$this->GRAPHIC->init($this->image);
-		if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::initImage()');
 	}
 	
 	/**
@@ -209,7 +186,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function checkPreflight() {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::checkPreflight()');
 		if(!$this->MAPCFG->BACKGROUND->checkFileExists(0)) {
 			$this->errorBox($this->LANG->getMessageText('backgroundNotExists','IMGPATH~'.$this->MAINCFG->getValue('paths', 'map').$this->MAPCFG->getImage()));
 		}
@@ -222,7 +198,6 @@ class NagVisBackground extends NagVisMap {
 		if(!$this->MAPOBJ->checkPermissions($this->MAPCFG->getValue('global',0, 'allowed_user'),0)) {
 			$this->errorBox($this->LANG->getMessageText('permissionDenied','USER~'.$this->MAINCFG->getRuntimeValue('user')));
 		}
-		if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::checkPreflight()');
 	}
 	
 	/**
@@ -232,7 +207,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function parseMap() {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::parseMap()');
 		switch($this->imageType) {
 			case 'jpg':
 				header('Content-type: image/jpeg');
@@ -243,7 +217,6 @@ class NagVisBackground extends NagVisMap {
 				header('Pragma: no-cache');
 				imagejpeg($this->image);
 				imagedestroy($this->image);
-				if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::parseMap()');
 				if (DEBUG&&DEBUGLEVEL&4) debugFinalize();
 			break;
 			case 'png':
@@ -255,12 +228,10 @@ class NagVisBackground extends NagVisMap {
 				header('Pragma: no-cache');
 				imagepng($this->image);
 				imagedestroy($this->image);
-				if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::parseMap()');
 				if (DEBUG&&DEBUGLEVEL&4) debugFinalize();
 			break;
 			default: 
 				// never reach this, error handling at the top
-				if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::parseMap(): Error');
 				if (DEBUG&&DEBUGLEVEL&4) debugFinalize();
 				exit;
 			break;
@@ -274,7 +245,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function errorBox($msg) {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::errorBox('.$msg.')');
 		$this->image = imagecreate(800,80);
 		$this->imageType = 'png';
 		$ImageFarbe = imagecolorallocate($this->image,243,243,243); 
@@ -282,7 +252,6 @@ class NagVisBackground extends NagVisMap {
 		$schrift = imagestring($this->image, 5,10, 10, $msg, $schriftFarbe);
 		
 		$this->parseMap();
-		if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::errorBox()');
 		exit;
 	}
 	
@@ -292,7 +261,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function parseObjects() {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::parseObjects()');
 		foreach($this->MAPOBJ->getMapObjects() AS $OBJ) {
 			switch(get_class($OBJ)) {
 				case 'NagVisMapObj':
@@ -314,7 +282,6 @@ class NagVisBackground extends NagVisMap {
 				break;
 			}
 		}
-		if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::parseObjects(): Array(...)');
 	}
 	
 	/**
@@ -325,7 +292,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function parseLine(&$OBJ) {
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::parseLine()');
 		if($OBJ->line_type == '10'){
 			list($x_from,$x_to) = explode(',', $OBJ->getX());
 			list($y_from,$y_to) = explode(',', $OBJ->getY());
@@ -340,7 +306,6 @@ class NagVisBackground extends NagVisMap {
 			
 			$this->GRAPHIC->drawArrow($this->image,$x_from,$y_from,$x_to,$y_to,3,1,$this->getColor($OBJ->getSummaryState()));
 		}
-		if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::parseLine()');	
 	}
 	
 	/**
@@ -351,7 +316,6 @@ class NagVisBackground extends NagVisMap {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function getColor($state){
-		if (DEBUG&&DEBUGLEVEL&1) debug('Start method NagVisBackground::getColor('.$state.')');
 		if($state == 'OK' || $state == 'UP') {
 			$color = $this->intOk;
 		} elseif($state == 'WARNING') {
@@ -361,7 +325,6 @@ class NagVisBackground extends NagVisMap {
 		} else {
 			$color = $this->intUnknown;
 		}
-		if (DEBUG&&DEBUGLEVEL&1) debug('End method NagVisBackground::getColor(): '.$color);
 		return $color;
 	}
 }
