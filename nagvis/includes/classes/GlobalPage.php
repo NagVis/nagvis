81c5ea38287a244a509ad7d3db9e86c82d3fe2e1
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-06-02 22:26:43 +0000

    Fixed problem where the preflight checks of wui pages were not done
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1321 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/GlobalPage.php
+++ nagvis/includes/classes/GlobalPage.php
@@ -73,7 +73,7 @@ class GlobalPage {
 		$this->user = $this->getUser();
 		$this->MAINCFG->setRuntimeValue('user',$this->user);
 		
-		$this->checkPreflight();
+		GlobalPage::checkPreflight();
 	}
 	
 	/**
@@ -83,10 +83,12 @@ class GlobalPage {
 	 * @author 	Lars Michelsen <lars@vertical-visions.de>
 	 */
 	function getUser() {
-		if(isset($_SERVER['PHP_AUTH_USER'])) {
+		if(isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_USER'] != '') {
 			return $_SERVER['PHP_AUTH_USER'];
-		} elseif(isset($_SERVER['REMOTE_USER'])) {
+		} elseif(isset($_SERVER['REMOTE_USER']) && $_SERVER['REMOTE_USER'] != '') {
 			return $_SERVER['REMOTE_USER'];
+		} else {
+			return FALSE;
 		}
 	}
 
