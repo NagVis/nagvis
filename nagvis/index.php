2ea8e8d76a750f84e70ed170d72aa25d0c7ac2af
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-06-28 18:02:33 +0000

    New index file with modifications from michael
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1398 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/index.php
+++ nagvis/index.php
@@ -3,7 +3,8 @@
  *
  * index.php - Main page of NagVis
  *
- * Copyright (c) 2004-2008 NagVis Project (Contact: lars@vertical-visions.de)
+ * Copyright (c) 2004-2008 NagVis Project (Contact: lars@vertical-visions.de
+ *                                                  , michael_luebben@web.de)
  *
  * License:
  *
@@ -25,125 +26,50 @@
 // Start the user session (This is needed by some caching mechanism)
 @session_start();
 
+// Set PHP error handling to standard level
+error_reporting(E_ALL ^ E_STRICT);
+
 // Include defines
-require("./includes/defines/global.php");
-require("./includes/defines/matches.php");
+require('./includes/defines/global.php');
+require('./includes/defines/matches.php');
 
 // Include functions
-require("./includes/functions/debug.php");
-require("./includes/functions/oldPhpVersionFixes.php");
+require('./includes/functions/debug.php');
+require('./includes/functions/oldPhpVersionFixes.php');
 
-// Include needed global classes
-require("./includes/classes/GlobalMainCfg.php");
-require("./includes/classes/GlobalMapCfg.php");
-require("./includes/classes/GlobalLanguage.php");
-require("./includes/classes/GlobalPage.php");
-require("./includes/classes/GlobalMap.php");
-require("./includes/classes/GlobalBackground.php");
-require("./includes/classes/GlobalGraphic.php");
-require("./includes/classes/GlobalBackendMgmt.php");
-
-// Include needed nagvis classes
-require("./includes/classes/NagVisMapCfg.php");
-require("./includes/classes/NagVisMap.php");
-require("./includes/classes/NagVisFrontend.php");
-require("./includes/classes/NagVisAutoMap.php");
-require("./includes/classes/NagVisInfoPage.php");
-
-// Include needed nagvis object classes
-require("./includes/classes/objects/NagVisObject.php");
-require("./includes/classes/objects/NagVisStatefulObject.php");
-require("./includes/classes/objects/NagVisStatelessObject.php");
-require("./includes/classes/objects/NagiosHost.php");
-require("./includes/classes/objects/NagVisHost.php");
-require("./includes/classes/objects/NagiosService.php");
-require("./includes/classes/objects/NagVisService.php");
-require("./includes/classes/objects/NagiosHostgroup.php");
-require("./includes/classes/objects/NagVisHostgroup.php");
-require("./includes/classes/objects/NagiosServicegroup.php");
-require("./includes/classes/objects/NagVisServicegroup.php");
-require("./includes/classes/objects/NagVisMapObj.php");
-require("./includes/classes/objects/NagVisShape.php");
-require("./includes/classes/objects/NagVisTextbox.php");
-
-// Load the main configuration
-$MAINCFG = new GlobalMainCfg(CONST_MAINCFG);
-
-// If not set, initialize the map var
-if(!isset($_GET['map'])) {
-	$_GET['map'] = '';
+/**
+ * Load required files
+ *
+ * @param	string	$class
+ * @author  Michael Luebben <michael_luebben@web.de>
+ */
+function __autoload($class) {
+	require($class.'.php');
 }
 
-// Initialize map configuration
-$MAPCFG = new NagVisMapCfg($MAINCFG,$_GET['map']);
-// Read the map configuration file
-$MAPCFG->readMapConfig();
+/**
+ * Sets the path where we will look for files when they
+ * are requested.
+ *
+ * @author  Michael Luebben <michael_luebben@web.de>
+ */
+set_include_path(
+	get_include_path()
+	.PATH_SEPARATOR.'./includes/classes/'
+	.PATH_SEPARATOR.'./includes/classes/objects/'
+	.PATH_SEPARATOR.'./includes/classes/controller/'
+	.PATH_SEPARATOR.'./includes/classes/validator/'
+	.PATH_SEPARATOR.'./includes/classes/httpRequest/'
+);
 
-// Initialize backend(s)
-$BACKEND = new GlobalBackendMgmt($MAINCFG);
+$controller = new GlobalController();
 
-// Initialize the frontend
-$FRONTEND = new NagVisFrontend($MAINCFG,$MAPCFG,$BACKEND);
-
-if(isset($_GET['map']) && $_GET['map'] != '') {
-	// Build the page
-	$FRONTEND->addBodyLines($FRONTEND->getRefresh());
-	$FRONTEND->getHeaderMenu();
-	$FRONTEND->getMap();
-	$FRONTEND->getMessages();
-} elseif(isset($_GET['url'])) {
-	// Build the page
-	$FRONTEND->addBodyLines($FRONTEND->getRefresh());
-	$FRONTEND->getHeaderMenu();
-	$arrFile = file($_GET['url']);
-	$FRONTEND->addBodyLines($arrFile);
-} elseif(isset($_GET['automap']) && $_GET['automap'] != '') {
-	// Initialize the possible vars
-	if(!isset($_GET['backend'])) {
-		$_GET['backend'] = '';
-	}
-	if(!isset($_GET['root'])) {
-		$_GET['root'] = '';
-	}
-	if(!isset($_GET['maxLayers'])) {
-		$_GET['maxLayers'] = '';
-	}
-	if(!isset($_GET['renderMode'])) {
-		$_GET['renderMode'] = '';
-	}
-	if(!isset($_GET['width'])) {
-		$_GET['width'] = '';
-	}
-	if(!isset($_GET['height'])) {
-		$_GET['height'] = '';
-	}
-	if(!isset($_GET['ignoreHosts'])) {
-		$_GET['ignoreHosts'] = '';
-	}
-	if(!isset($_GET['filterGroup'])) {
-		$_GET['filterGroup'] = '';
-	}
-	
-	// Build the page
-	$FRONTEND->addBodyLines($FRONTEND->getRefresh());
-	$FRONTEND->getHeaderMenu();
-	$FRONTEND->getAutoMap(Array('backend' => $_GET['backend'], 'root' => $_GET['root'], 'maxLayers' => $_GET['maxLayers'], 'renderMode' => $_GET['renderMode'], 'ignoreHosts' => $_GET['ignoreHosts'], 'filterGroup' => $_GET['filterGroup'], 'width' => $_GET['width'], 'height' => $_GET['height']));
-	$FRONTEND->getMessages();
-} elseif(isset($_GET['rotation']) && $_GET['rotation'] != '' && (!isset($_GET['url']) || $_GET['url'] == '') && (!isset($_GET['map']) || $_GET['map'] == '')) {
-	// Redirect to next page
-	header('Location: '.$FRONTEND->getNextRotationUrl());
-} elseif(isset($_GET['info'])) {
-		// Build and view the information page
-		print new NagVisInfoPage($MAINCFG);
-		exit;
-} else {
-	// Build the page
-	$FRONTEND->addBodyLines($FRONTEND->getRefresh());
-	$FRONTEND->getHeaderMenu();
-	$FRONTEND->addBodyLines($FRONTEND->getIndexPage());
-	$FRONTEND->getMessages();
-}
-
-// Print the page
-$FRONTEND->printPage();
+// ONLY FOR DEBUGGING
+//-----------------------------------------------------------------------------------------------
+$state = ($controller->isValid()) ? 'TRUE' : 'FALSE';
+print "Status        : <b>${state}</b><br />";
+print "Action        : ".$controller->getAction()."<br />";
+print "Parametername : ".$controller->getParameterName()."<br />";
+print "Message       : ".$controller->getMessage();
+//-----------------------------------------------------------------------------------------------
 ?>
