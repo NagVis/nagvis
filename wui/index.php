9443fe288a9b10bb184f94367fcd0683ce99c910
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-07-31 19:55:40 +0000

    Added standard error reporting to wui pages
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1498 ff461055-cf1a-0410-99e4-fa365203d4c3

--- wui/index.php
+++ wui/index.php
@@ -28,6 +28,9 @@
 // Start the user session (This is needed by some caching mechanism)
 @session_start();
 
+// Set PHP error handling to standard level
+error_reporting(E_ALL ^ E_STRICT);
+
 // Include defines
 require("../nagvis/includes/defines/global.php");
 require("../nagvis/includes/defines/matches.php");
