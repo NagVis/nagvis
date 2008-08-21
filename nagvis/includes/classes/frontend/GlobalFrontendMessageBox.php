17eb77def7d3afe506125c31186fb563fd8212ce
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-08-21 21:27:43 +0000

    Some tuning on error message html code and stylesheets
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1613 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/classes/frontend/GlobalFrontendMessageBox.php
+++ nagvis/includes/classes/frontend/GlobalFrontendMessageBox.php
@@ -110,49 +110,26 @@ class GlobalFrontendMessageBox {
 	 * @author  Michael Luebben <michael_luebben@web.de>
 	 */
 	private function buildMessageBox() {
-		/*$this->page  = '<html>'."\n";
-		$this->page .= '   <head>'."\n";
-		$this->page .= '      <meta http-equiv="Content-Type" content="text/html;charset=utf-8">'."\n";
-		$this->page .= '      <title>'.$this->title.'</title></head>'."\n";
-		$this->page .= '   </head>'."\n";
-		$this->page .= '   <body>'."\n";*/
-		$this->page .= '      <style type="text/css"><!-- @import url('.$this->pathHtmlBase.'/nagvis/includes/css/frontendMessage.css);  --></style>'."\n";
-		$this->page .= '      <div id="messageBoxDiv" class="shadow">'."\n";
-		$this->page .= '         <table class="'.$this->type.'MessageBoxBorder" height="100%" border="1" cellpadding="0" cellspacing="0">'."\n";
-		$this->page .= '            <tr>'."\n";
-		$this->page .= '               <td class="'.$this->type.'MessageBoxBackground">'."\n";
-		$this->page .= '                  <table cellpadding="0" cellspacing="0" width="100%" height="10">'."\n";
-		$this->page .= '                     <tr>'."\n";
-		$this->page .= '                        <td  class="'.$this->type.'MessageBoxBackground">'."\n";
-		$this->page .= '                        </td>'."\n";
-		$this->page .= '                     </tr>'."\n";
-		$this->page .= '                  </table>'."\n";
-		$this->page .= '                  <table cellpadding="0" cellspacing="0" width="100%">'."\n";
-		$this->page .= '                     <tr>'."\n";
-		$this->page .= '                        <td class="'.$this->type.'MessageHeader" align="center" width="60">'."\n";
-		$this->page .= '                           <img src="'.$this->pathHtmlBase.'/nagvis/images/internal/msg_'.$this->type.'.png"/>'."\n";
-		$this->page .= '                        </td>'."\n";
-		$this->page .= '                        <td id="messageHeader" class="'.$this->type.'MessageHeader">'."\n";
-		$this->page .=                             $this->title."\n";
-		$this->page .= '                        </td>'."\n";
-		$this->page .= '                        <td class="'.$this->type.'MessageHeader" align="center" width="60">'."\n";
-		$this->page .= '                           <img src="'.$this->pathHtmlBase.'/nagvis/images/internal/msg_'.$this->type.'.png"/>'."\n";
-		$this->page .= '                        </td>'."\n";
-		$this->page .= '                     </tr>'."\n";
-		$this->page .= '                  </table>'."\n";
-		$this->page .= '                  <table cellpadding="0" cellspacing="0" width="100%" height="78%">'."\n";
-		$this->page .= '                     <tr>'."\n";
-		$this->page .= '                        <td class="'.$this->type.'MessageBoxBackground">'."\n";
-		$this->page .=                             $this->message."\n";
-		$this->page .= '                        </td>'."\n";
-		$this->page .= '                     </tr>'."\n";
-		$this->page .= '                  </table>'."\n";
-		$this->page .= '               </td>'."\n";
-		$this->page .= '            </tr>'."\n";
-		$this->page .= '         </table>'."\n";
-		$this->page .= '      </div>'."\n";
-		/*$this->page .= '   </body>'."\n";
-		$this->page .= '</html>'."\n";*/
+		$this->page .= '<style type="text/css"><!-- @import url('.$this->pathHtmlBase.'/nagvis/includes/css/frontendMessage.css);  --></style>'."\n";
+		$this->page .= '<div id="messageBoxDiv" class="shadow">'."\n";
+		$this->page .= '   <table id="messageBox" class="'.$this->type.'" height="100%" cellpadding="0" cellspacing="0">'."\n";
+		$this->page .= '      <tr>'."\n";
+		$this->page .= '         <td class="'.$this->type.'" colspan="3" height="16">'."\n";
+		$this->page .= '      </tr>'."\n";
+		$this->page .= '      <tr height="32">'."\n";
+		$this->page .= '         <th class="'.$this->type.'" align="center" width="60">'."\n";
+		$this->page .= '           <img src="'.$this->pathHtmlBase.'/nagvis/images/internal/msg_'.$this->type.'.png"/>'."\n";
+		$this->page .= '         </th>'."\n";
+		$this->page .= '         <th class="'.$this->type.'">'.$this->title.'</td>'."\n";
+		$this->page .= '         <th class="'.$this->type.'" align="center" width="60">'."\n";
+		$this->page .= '           <img src="'.$this->pathHtmlBase.'/nagvis/images/internal/msg_'.$this->type.'.png"/>'."\n";
+		$this->page .= '         </th>'."\n";
+		$this->page .= '       </tr>'."\n";
+		$this->page .= '       <tr>'."\n";
+		$this->page .= '         <td class="'.$this->type.'" colspan="3" style="padding-top:16px;">'.$this->message.'</td>'."\n";
+		$this->page .= '       </tr>'."\n";
+		$this->page .= '   </table>'."\n";
+		$this->page .= '</div>'."\n";
 	}
 
 	/**
