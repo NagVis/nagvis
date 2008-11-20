640c8e3e03a9a1e81699aa2a7794a072c62bc3cb
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-11-20 15:23:52 +0000

    Fixed background color of hover menu
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1849 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/js/overlib.js
+++ nagvis/includes/js/overlib.js
@@ -36,8 +36,8 @@ registerCommands('donothing,inarray,caparray,sticky,background,noclose,caption,l
 // Settings you want everywhere are set here. All of this can also be
 // changed on your html page or through an overLIB call.
 ////////
-if (typeof ol_fgcolor=='undefined') var ol_fgcolor="#CCCCFF";
-if (typeof ol_bgcolor=='undefined') var ol_bgcolor="#333399";
+if (typeof ol_fgcolor=='undefined') var ol_fgcolor="#FFFFFF";
+if (typeof ol_bgcolor=='undefined') var ol_bgcolor="#FFFFFF";
 if (typeof ol_textcolor=='undefined') var ol_textcolor="#000000";
 if (typeof ol_capcolor=='undefined') var ol_capcolor="#FFFFFF";
 if (typeof ol_closecolor=='undefined') var ol_closecolor="#9999FF";
