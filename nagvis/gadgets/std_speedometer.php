efddfcfbc387e442cf512cd5370db18a8d19d212
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2008-12-09 17:30:09 +0000

    Fixed errorbox - Thanks Sascha
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@1877 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/gadgets/std_speedometer.php
+++ nagvis/gadgets/std_speedometer.php
@@ -128,7 +128,7 @@ function errorBox($msg) {
 	$img = imagecreate(400,40);
 	
 	$bgColor = imagecolorallocate($img, 255, 255, 255);
-	imagefill($img, 0, 0, $oBackground);
+	imagefill($img, 0, 0, $bgColor);
 	
 	$fontColor = imagecolorallocate($img, 10, 36, 106);
 	imagestring($img, 2, 8, 8, $msg, $fontColor);
