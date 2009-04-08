fac9e4223a5a7f29ae13684694b3c8f8a9eecc7a
larsi- <larsi-@ff461055-cf1a-0410-99e4-fa365203d4c3>
2009-04-08 21:00:15 +0000

    Fixed some coding style
    git-svn-id: file:///root/orig/nagvis/nagvis/trunk@2171 ff461055-cf1a-0410-99e4-fa365203d4c3

--- nagvis/includes/js/nagvis.js
+++ nagvis/includes/js/nagvis.js
@@ -585,17 +585,19 @@ function oDump(object, depth, max){
 	depth = depth || 0;
 	max = max || 2;
 	
-	if (depth > max)
+	if (depth > max) {
 		return false;
+	}
 	
 	var indent = "";
-	for (var i = 0; i < depth; i++)
+	for (var i = 0; i < depth; i++) {
 		indent += "  ";
+	}
 	
 	var output = "";  
-	for (var key in object){
+	for (var key in object) {
 		output += "\n" + indent + key + ": ";
-		switch (typeof object[key]){
+		switch (typeof object[key]) {
 			case "object": output += oDump(object[key], depth + 1, max); break;
 			case "function": output += "function"; break;
 			default: output += object[key]; break;        
