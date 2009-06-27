94570458fd37d7b18f360d790df54cc79e08399a
larsi- <lars@vertical-visions.de>
2009-06-27 19:18:50 +0200

    Splitted automap configuration files from maps directory to separate etc/automaps directory; Automaps are listed separate on overview page now; There is an option for toggle the listing of automaps on the overview page now

---
+++ nagvis/nagvis/includes/classes/NagVisAutomapCfg.php
@@ -0,0 +1,48 @@
+<?php
+/*****************************************************************************
+ *
+ * NagVisAutomapCfg.php - Class for handling the NagVis automap configuration
+ * files
+ *
+ * Copyright (c) 2004-2009 NagVis Project (Contact: lars@vertical-visions.de)
+ *
+ * License:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *****************************************************************************/
+ 
+/**
+ * @author	Lars Michelsen <lars@vertical-visions.de>
+ */
+class NagVisAutomapCfg extends GlobalMapCfg {
+	/**
+	 * Class Constructor
+	 *
+	 * @param	GlobalCore      $CORE
+	 * @param	String			$name		Name of the map
+	 * @author	Lars Michelsen <lars@vertical-visions.de>
+	 */
+	public function __construct($CORE, $name) {
+		// Start of the parent constructor
+		parent::__construct($CORE, $name);
+		
+		// Override the default map configuration path with automap path
+		$this->setConfigFile($CORE->MAINCFG->getValue('paths', 'automapcfg').$name.'.cfg');
+		
+		// Re-initialize the cache
+		$this->initCache();
+	}
+}
+?>
