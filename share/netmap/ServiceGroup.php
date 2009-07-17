434d9a059a881eaad146eb634a6932e9f89dd21f
Roman Kyrylych <op5-team@users.sourceforge.net>
2009-07-17 16:36:21 +0300

    Added forgotten files for {host,service}group classes
    Signed-off-by: Roman Kyrylych <op5-team@users.sourceforge.net>

---
+++ share/netmap/ServiceGroup.php
@@ -0,0 +1,40 @@
+<?php
+
+/*****************************************************************************
+ *
+ * Copyright (C) 2009 NagVis Project
+ *
+ * License:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *****************************************************************************/
+
+class ServiceGroup
+{
+	public $id;
+	public $name;
+	public $alias;
+	public $selected;
+
+	public function __construct($id, $name, $alias = '', $selected = false)
+	{
+		$this->id = $id;
+		$this->name = $name;
+		$this->alias = $alias;
+		$this->selected = $selected;
+	}
+}
+
+?>
