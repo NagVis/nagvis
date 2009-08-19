4baaf392a0cd17c3517b02a2b228b0126ae9b68d
Roman Kyrylych <op5-team@users.sourceforge.net>
2009-08-19 16:36:01 +0300

    gmap: Added NagVisService and NagVisMapsData
    Signed-off-by: Roman Kyrylych <op5-team@users.sourceforge.net>

---
+++ share/netmap/NagVisService.php
@@ -0,0 +1,51 @@
+<?php
+
+/*****************************************************************************
+ *
+ * Copyright (C) 2009 NagVis Project
+ *
+ * License:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *****************************************************************************/
+
+class NagVisService
+{
+	private $CORE;
+
+	private function init()
+	{
+		require_once("../nagvis/includes/defines/global.php");
+		require_once("../nagvis/includes/defines/matches.php");
+		set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__))
+			. PATH_SEPARATOR . '../nagvis/includes/classes/'
+			. PATH_SEPARATOR . '../nagvis/includes/classes/validator/'
+			. PATH_SEPARATOR . '../nagvis/includes/classes/frontend/');
+		require_once("../nagvis/includes/functions/oldPhpVersionFixes.php");
+		require_once("../nagvis/includes/functions/getuser.php");
+
+		$this->CORE = new GlobalCore();
+		$this->CORE->MAINCFG->setRuntimeValue('user', getUser());
+	}
+
+	public function getMaps()
+	{
+		$this->init();
+		$maps = $this->CORE->getAvailableMaps();
+		return array_values($maps);
+	}
+}
+
+?>
