0e07621e115efc878e8c547db03de186b42cc228
Lars Michelsen <lars@vertical-visions.de>
2009-09-04 16:27:09 +0200

    #17 modified redirect files to new directory structure

--- share/config.php
+++ share/config.php
@@ -25,5 +25,5 @@
 header("Location: ".((isset($_SERVER["HTTPS"])) ? 'https://': 'http://') . $_SERVER['HTTP_HOST'] 
 	. ((isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != '80') ? ':'.$_SERVER['SERVER_PORT']: '')
 	. rtrim(dirname($_SERVER['PHP_SELF']), '/\\')
-    . "/wui/index.php".(($_SERVER["QUERY_STRING"] != '') ? '?':'').$_SERVER["QUERY_STRING"]);
+    . "/frontend/wui/index.php".(($_SERVER["QUERY_STRING"] != '') ? '?':'').$_SERVER["QUERY_STRING"]);
 ?>
--- share/index.php
+++ share/index.php
@@ -25,5 +25,5 @@
 header("Location: ". ((isset($_SERVER["HTTPS"])) ? 'https://': 'http://') . $_SERVER['HTTP_HOST'] 
 	. ((isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != '80') ? ':'.$_SERVER['SERVER_PORT']: '')
 	. rtrim(dirname($_SERVER['PHP_SELF']), '/\\')
-    . "/nagvis/index.php".(($_SERVER["QUERY_STRING"] != '') ? '?':'').$_SERVER["QUERY_STRING"]);
+    . "/frontend/nagvis-js/index.php".(($_SERVER["QUERY_STRING"] != '') ? '?':'').$_SERVER["QUERY_STRING"]);
 ?>
