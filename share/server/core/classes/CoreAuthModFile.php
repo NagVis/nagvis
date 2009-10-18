0f89ebbe50cb37a16ebe845cd1a516360880c6d3
Lars Michelsen <lars@vertical-visions.de>
2009-10-18 22:29:54 +0200

    Added document headers to serveral new files

--- share/server/core/classes/CoreAuthModFile.php
+++ share/server/core/classes/CoreAuthModFile.php
@@ -1,5 +1,30 @@
 <?php
-
+/*******************************************************************************
+ *
+ * CoreAuthModFile.php - Authentication module for file based authentication
+ *
+ * Copyright (c) 2004-2009 NagVis Project (Contact: info@nagvis.org)
+ *
+ * License:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ ******************************************************************************/
+ 
+/**
+ * @author Lars Michelsen <lars@vertical-visions.de>
+ */
 class CoreAuthModFile extends CoreAuthModule {
 	private $CORE;
 	private $USERCFG;
--- share/server/core/classes/CoreAuthorisationModule.php
+++ share/server/core/classes/CoreAuthorisationModule.php
@@ -1,4 +1,27 @@
 <?php
+/*******************************************************************************
+ *
+ * CoreAuthorisationModule.php - Abstract authorisation module
+ *
+ * Copyright (c) 2004-2009 NagVis Project (Contact: info@nagvis.org)
+ *
+ * License:
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ ******************************************************************************/
+
 /**
  * Abstract definition of a CoreAuthorisationModule
  * All authorisaiton modules should extend this class
