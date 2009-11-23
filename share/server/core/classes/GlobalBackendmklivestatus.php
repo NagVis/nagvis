750b3c9a2271f2745becce7294d48ebb1f65b1d7
LaMi <lars@vertical-visions.de>
2009-11-23 10:13:05 +0100

    Fixed encoding problem

--- share/server/core/classes/GlobalBackendmklivestatus.php
+++ share/server/core/classes/GlobalBackendmklivestatus.php
@@ -220,7 +220,7 @@ class GlobalBackendmklivestatus implements GlobalBackendInterface {
 		}
 		
 		// Decode the json response
-		$obj = json_decode($read);
+		$obj = json_decode(utf8_encode($read));
 		
 		// TEST: Disable KeepAlive:
 		//socket_close($this->SOCKET);
