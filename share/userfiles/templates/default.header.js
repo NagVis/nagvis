5ac6d8f6acb083db4a5a1d9cf6cadcd11ca1cd53
LaMi <lars@vertical-visions.de>
2010-01-14 17:40:03 +0100

    Moved most important menus from WUI context menu to header menu so the WUI context navigation will be removed soon

--- share/userfiles/templates/default.header.js
+++ share/userfiles/templates/default.header.js
@@ -28,58 +28,83 @@
 var DDSPEED = 10;
 var DDTIMER = 15;
 
+// Hide the given menus instant
+function ddMenuHide(aIds) {
+	var h;
+	var c;
+	for(var i = 0; i < aIds.length; i++) {
+		var h = document.getElementById(aIds[i] + '-ddheader');
+		var c = document.getElementById(aIds[i] + '-ddcontent');
+		
+		clearInterval(c.timer);
+		clearTimeout(h.timer);
+		
+		c.style.height = '0px';
+	}
+	
+	c = null;
+	h = null;
+}
+
 // main function to handle the mouse events //
-function ddMenu(id,d){
-  var h = document.getElementById(id + '-ddheader');
-  var c = document.getElementById(id + '-ddcontent');
-  clearInterval(c.timer);
-  if(d == 1){
-    clearTimeout(h.timer);
-    if(c.maxh && c.maxh <= c.offsetHeight){return}
-    else if(!c.maxh){
-      c.style.display = 'inline';
-      c.style.height = 'auto';
-      c.maxh = c.offsetHeight;
-      c.style.height = '0px';
-    }
-    c.timer = setInterval(function(){ddSlide(c,1)},DDTIMER);
-  }else{
-    h.timer = setTimeout(function(){ddCollapse(c)},50);
-  }
+function ddMenu(id, d, reposition){
+	var h = document.getElementById(id + '-ddheader');
+	var c = document.getElementById(id + '-ddcontent');
+
+	// Reposition by trigger object when some given (used on submenus)
+	if(typeof reposition !== 'undefined' && d == 1) {
+		c.style.left = (h.offsetWidth-1)+"px";
+		c.style.top = (h.offsetTop)+"px";
+	}
+
+	clearInterval(c.timer);
+	if(d == 1){
+		clearTimeout(h.timer);
+		if(c.maxh && c.maxh <= c.offsetHeight){return}
+		else if(!c.maxh){
+			c.style.display = 'inline';
+			c.style.height = 'auto';
+			c.maxh = c.offsetHeight;
+			c.style.height = '0px'; 
+		}
+		c.timer = setInterval(function(){ddSlide(c,1)},DDTIMER);
+	}else{
+		h.timer = setTimeout(function(){ddCollapse(c)},50);
+	}
 }
 
 // collapse the menu //
 function ddCollapse(c){
-  c.timer = setInterval(function(){ddSlide(c,-1)},DDTIMER);
+	c.timer = setInterval(function(){ddSlide(c,-1)},DDTIMER);
 }
 
 // cancel the collapse if a user rolls over the dropdown //
 function cancelHide(id){
-  var h = document.getElementById(id + '-ddheader');
-  var c = document.getElementById(id + '-ddcontent');
-  clearTimeout(h.timer);
-  clearInterval(c.timer);
-  if(c.offsetHeight < c.maxh){
-    c.timer = setInterval(function(){ddSlide(c,1)},DDTIMER);
-  }
+	var h = document.getElementById(id + '-ddheader');
+	var c = document.getElementById(id + '-ddcontent');
+	clearTimeout(h.timer);
+	clearInterval(c.timer);
+	if(c.offsetHeight < c.maxh){
+		c.timer = setInterval(function(){ddSlide(c,1)},DDTIMER);
+	}
 }
 
 // incrementally expand/contract the dropdown and change the opacity //
 function ddSlide(c,d){
-  var currh = c.offsetHeight;
-  var dist;
-  if(d == 1){
-    dist = (Math.round((c.maxh - currh) / DDSPEED));
-  }else{
-    dist = (Math.round(currh / DDSPEED));
-  }
-  if(dist <= 1 && d == 1){
-    dist = 1;
-  }
-  c.style.height = currh + (dist * d) + 'px';
-  c.style.opacity = currh / c.maxh;
-  c.style.filter = 'alpha(opacity=' + (currh * 100 / c.maxh) + ')';
-  if((currh < 2 && d != 1) || (currh > (c.maxh - 2) && d == 1)){
-    clearInterval(c.timer);
-  }
+	var currh = c.offsetHeight;
+	var dist;
+	if(d == 1){
+		dist = (Math.round((c.maxh - currh) / DDSPEED));
+	}else{
+		dist = (Math.round(currh / DDSPEED));
+	}
+	if(dist <= 1 && d == 1){
+		dist = 1;
+	}
+	c.style.height = currh + (dist * d) + 'px';
+	c.style.opacity = currh / c.maxh;
+	c.style.filter = 'alpha(opacity=' + (currh * 100 / c.maxh) + ')';
+	if((currh < 2 && d != 1) || (currh > (c.maxh - 2) && d == 1)){
+		clearInterval(c.timer);
+	}
 }
