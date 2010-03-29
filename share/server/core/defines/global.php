7b1a89d6226e4c4f182630a81b5fb5f1711a1c80
LaMi <lars@vertical-visions.de>
2010-03-29 23:26:18 +0200

    Updated version

--- share/server/core/defines/global.php
+++ share/server/core/defines/global.php
@@ -53,7 +53,7 @@ define('DEBUGLEVEL', 4);
 define('DEBUGFILE', '../../../var/nagvis-debug.log');
 
 // NagVis Version
-define('CONST_VERSION', '1.5b2');
+define('CONST_VERSION', '1.5b3');
 
 // Path to the main configuration file
 define('CONST_MAINCFG', '../../../etc/nagvis.ini.php');
