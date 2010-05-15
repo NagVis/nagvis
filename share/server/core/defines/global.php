6d9c120c50ba031557d8db52538122212a955155
Lars Michelsen <lm@larsmichelsen.com>
2010-05-15 13:45:17 +0200

    new version tag

--- share/server/core/defines/global.php
+++ share/server/core/defines/global.php
@@ -58,7 +58,7 @@ define('DEBUGLEVEL', 6);
 define('DEBUGFILE', '../../../var/nagvis-debug.log');
 
 // NagVis Version
-define('CONST_VERSION', '1.5b4');
+define('CONST_VERSION', '1.5rc1');
 
 // Path to the main configuration file
 define('CONST_MAINCFG', '../../../etc/nagvis.ini.php');
