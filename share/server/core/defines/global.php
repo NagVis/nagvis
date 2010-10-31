8f4ab41208d3d9df20dcdd80e50eaf080acd2a04
Lars Michelsen <lm@larsmichelsen.com>
2010-10-31 23:59:30 +0100

    Updated version

--- share/server/core/defines/global.php
+++ share/server/core/defines/global.php
@@ -27,7 +27,7 @@
  */
 
 // NagVis Version
-define('CONST_VERSION', '1.5.4');
+define('CONST_VERSION', '1.6a1');
 
 // Set PHP error handling to standard level
 // Different levels for php versions below 5.1 because PHP 5.1 reports
