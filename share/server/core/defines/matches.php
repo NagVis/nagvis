a106c65a3a4954f4b7fc31b5bd406ea41fba4684
Lars Michelsen <lm@larsmichelsen.com>
2011-02-01 18:14:40 +0100

    Allowing # chars in style/label style attributes now
    Conflicts:
    
    	share/server/core/defines/matches.php

--- share/server/core/defines/matches.php
+++ share/server/core/defines/matches.php
@@ -37,7 +37,7 @@ define('MATCH_STRING_PATH', '/^[0-9a-z\s\_\.\-\/\\\]+$/i');
 define('MATCH_STRING_URL', '/^[0-9a-z\s\:\+\[\]\(\)\=\%\?\&\_\,\.\-\#\@\=\/\\\]+$/i');
 define('MATCH_STRING_URL_EMPTY', '/^[0-9a-z\s\:\+\[\]\(\)\=\%\?\&\_\,\.\-\#\@\=\/\\\]*$/i');
 define('MATCH_GADGET_OPT', '/^[0-9a-z\s\:\+\[\]\(\)\_\.\,\-\&\?\!\#\@\=\/\\\%]+$/i');
-define('MATCH_STRING_STYLE', '/^[0-9a-z\:\;\-\+\%]*$/i');
+define('MATCH_STRING_STYLE', '/^[0-9a-z\:\;\-\+\%\#]*$/i');
 define('MATCH_COORDS',       '/^(?:(?:[0-9]+)|([a-z0-9]+(?:%[+-][0-9]+)?))$/');
 define('MATCH_COORDS_MULTI', '/^(?:(?:(?:[0-9]+)|([a-z0-9]+(?:%[+-][0-9]+)?))[\.\,]?)+$/');
 
