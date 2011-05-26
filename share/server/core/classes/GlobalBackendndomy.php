8714137ad56738d30da931b6fa4a2e56e9f3e41f
Lars Michelsen <lm@larsmichelsen.com>
2011-05-26 21:56:32 +0200

    Bugfix: NDO Backend: Fixed wrong SQL syntax in getHostState function

--- share/server/core/classes/GlobalBackendndomy.php
+++ share/server/core/classes/GlobalBackendndomy.php
@@ -519,7 +519,7 @@ class GlobalBackendndomy implements GlobalBackendInterface {
             problem_has_been_acknowledged,
             statusmap_image,
             UNIX_TIMESTAMP(last_check) AS last_check, UNIX_TIMESTAMP(next_check) AS next_check,
-            hs.state_type, hs.current_check_attempt, hs.max_check_attempts, hs.check_command
+            hs.state_type, hs.current_check_attempt, hs.max_check_attempts, hs.check_command,
             UNIX_TIMESTAMP(dh.scheduled_start_time) AS downtime_start, UNIX_TIMESTAMP(dh.scheduled_end_time) AS downtime_end,
             dh.author_name AS downtime_author, dh.comment_data AS downtime_data
         FROM
