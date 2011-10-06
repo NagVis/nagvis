3aea2dbdd1ed8b1ad282df8fd40b2778f8d9ffc1
Lars Michelsen <lm@larsmichelsen.com>
2011-10-06 15:52:13 +0200

    Bugfix: IE8 fixing event registration (javascript errors on page loading)

--- share/frontend/nagvis-js/js/nagvis.js
+++ share/frontend/nagvis-js/js/nagvis.js
@@ -830,13 +830,7 @@ function addEvent(obj, type, fn) {
    if(obj.addEventListener) {
       obj.addEventListener(type, fn, false);
    } else if (obj.attachEvent) {
-      obj["e"+type+fn] = fn;
-
-      obj[type+fn] = function() {
-      	obj["e"+type+fn](window.event);
-      }
-
-      obj.attachEvent("on"+type, obj[type+fn]);
+      obj.attachEvent("on"+type, fn);
    }
 }
 
@@ -845,9 +839,7 @@ function removeEvent(obj, type, fn) {
     if(obj.removeEventListener) {
         obj.removeEventListener(type, fn, false);
     } else if (obj.detachEvent) {
-        obj.detachEvent("on"+type, obj[type+fn]);
-        delete obj["e"+type+fn];
-        delete obj[type+fn];
+        obj.detachEvent("on"+type, fn);
     }
 }
 
