2706005ee0f3d54065e7869b016b8612759a9f8c
Lars Michelsen <lm@larsmichelsen.com>
2012-01-22 12:58:25 +0100

    Increased version to 1.6.3

--- share/server/core/defines/global.php
+++ share/server/core/defines/global.php
@@ -27,7 +27,7 @@
  */
 
 // NagVis Version
-define('CONST_VERSION', '1.6.2');
+define('CONST_VERSION', '1.6.3');
 
 // Set PHP error handling to standard level
 // Different levels for php versions below 5.1 because PHP 5.1 reports
