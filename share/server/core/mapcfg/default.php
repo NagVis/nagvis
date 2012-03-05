64342ce06de13da9906fa911ec943327f8656ad9
Lars Michelsen <lm@larsmichelsen.com>
2012-03-05 20:00:21 +0100

    Bugfix: Stateless lines - only showing the hover area when it is needed
    Conflicts:
    
    	share/frontend/nagvis-js/js/NagVisObject.js

--- share/server/core/mapcfg/default.php
+++ share/server/core/mapcfg/default.php
@@ -203,10 +203,11 @@ $mapConfigVars = Array(
     ),
 
     'context_menu' => Array(
-        'must' => 0,
-        'default' => cfg('defaults', 'contextmenu'),
-        'match' => MATCH_BOOLEAN,
-        'field_type' => 'boolean'),
+        'must'       => 0,
+        'default'    => cfg('defaults', 'contextmenu'),
+        'match'      => MATCH_BOOLEAN,
+        'field_type' => 'boolean',
+    ),
     'context_template' => Array(
         'must'          => 0,
         'default'       => cfg('defaults', 'contexttemplate'),
@@ -799,6 +800,12 @@ $mapConfigVars = Array(
     ),
 );
 
+// STATELESS LINE SPECIFIC OPTIONS
+$mapConfigVars['context_menu_line'] = $mapConfigVars['context_menu'];
+$mapConfigVars['context_menu_line']['default'] = '0';
+$mapConfigVars['hover_menu_line'] = $mapConfigVars['hover_menu'];
+$mapConfigVars['hover_menu_line']['default'] = '0';
+
 //
 // map configuration variable registration
 //
@@ -1096,9 +1103,9 @@ $mapConfigVarMap['line'] = Array(
     'line_width' => null,
     'line_color' => null,
     'line_color_border' => null,
-    'context_menu' => null,
+    'context_menu_line' => 'context_menu',
     'context_template' => null,
-    'hover_menu' => null,
+    'hover_menu_line' => 'hover_menu',
     'hover_template' => null,
     'hover_url' => null,
     'hover_delay' => null,
