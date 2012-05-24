58a8d928dab4a62fa3ffce8f7863de4a3bface12
Lars Michelsen <lm@larsmichelsen.com>
2012-05-24 22:15:58 +0200

    Installer: Automatically removing showautomaps option from nagvis.ini.php during update

--- install.sh
+++ install.sh
@@ -1583,6 +1583,10 @@ if [ "$INSTALLER_ACTION" = "update" -a "$NAGVIS_VER_OLD" != "UNKNOWN" -a "$INSTA
         sed -i '/^wuijs=/d' $NAGVIS_PATH/etc/nagvis.ini.php
         chk_rc "| Error" "$DONE"
 
+        DONE=`log "Removing showautomaps option from main config..." done`
+        sed -i '/^showautomaps=/d' $NAGVIS_PATH/etc/nagvis.ini.php
+        chk_rc "| Error" "$DONE"
+
         # Remove base and htmlbase path from cross path updated main
         # configuration file
         if [ "$NAGVIS_PATH_OLD" != "$NAGVIS_PATH" ]; then
