110520066d805cf09a7b0f089701e094dcaff8e4
Lars Michelsen <lm@larsmichelsen.com>
2015-03-07 19:15:38 +0100

    Migrated "manage shape" dialog to new form dialog handling

--- share/server/core/classes/CoreModManageShapes.php
+++ share/server/core/classes/CoreModManageShapes.php
@@ -34,11 +34,7 @@ class CoreModManageShapes extends CoreModule {
 
         // Register valid actions
         $this->aActions = Array(
-            // WUI specific actions
             'view'      => 'manage',
-            'checkUsed' => 'manage',
-            'doUpload'  => 'manage',
-            'doDelete'  => 'manage',
         );
     }
 
@@ -47,92 +43,14 @@ class CoreModManageShapes extends CoreModule {
 
         if($this->offersAction($this->sAction)) {
             switch($this->sAction) {
-                case 'checkUsed':
-                    $sReturn = json_encode($this->checkUsed());
-                break;
                 case 'view':
                     $VIEW = new ViewManageShapes();
                     $sReturn = json_encode(Array('code' => $VIEW->parse()));
-                break;
-                case 'doDelete':
-                    $this->handleResponse('handleResponseDelete', 'doDelete',
-                                            l('The shape has been deleted.'),
-                                                                l('The shape could not be deleted.'),
-                                          1);
-                break;
-                case 'doUpload':
-                    if($this->handleResponse('handleResponseDoUpload', 'doUpload'))
-                        header('Location:'.$_SERVER['HTTP_REFERER']);
                 break;
             }
         }
 
         return $sReturn;
-    }
-
-    protected function handleResponseDoUpload() {
-        $FHANDLER = new CoreRequestHandler($_FILES);
-        $this->verifyValuesSet($FHANDLER, Array('image_file'));
-        return Array('image_file' => $FHANDLER->get('image_file'));
-    }
-
-    protected function doUpload($a) {
-        if(!is_uploaded_file($a['image_file']['tmp_name']))
-            throw new NagVisException(l('The file could not be uploaded (Error: [ERROR]).',
-              Array('ERROR' => $a['image_file']['error'].': '.$this->CORE->getUploadErrorMsg($a['image_file']['error']))));
-
-        $fileName = $a['image_file']['name'];
-
-        if(!preg_match(MATCH_PNG_GIF_JPG_FILE, $fileName))
-            throw new NagVisException(l('The uploaded file is no image (png,jpg,gif) file or contains unwanted chars.'));
-
-        $filePath = path('sys', '', 'shapes').$fileName;
-        return move_uploaded_file($a['image_file']['tmp_name'], $filePath) && $this->CORE->setPerms($filePath);
-    }
-
-    protected function doDelete($a) {
-        $path = path('sys', '', 'shapes', $a['image']);
-        if($path !== '')
-            return unlink($path);
-        else
-            return false;
-    }
-
-    protected function handleResponseDelete() {
-        $FHANDLER = new CoreRequestHandler($_POST);
-        $attr = Array('image' => MATCH_PNG_GIF_JPG_FILE);
-        $this->verifyValuesSet($FHANDLER,   $attr);
-        $this->verifyValuesMatch($FHANDLER, $attr);
-
-        if(!in_array($FHANDLER->get('image'), $this->CORE->getAvailableShapes()))
-            throw new NagVisException(l('The shape does not exist.'));
-
-        return Array('image' => $FHANDLER->get('image'));
-    }
-
-    protected function checkUsed() {
-        $FHANDLER = new CoreRequestHandler($_GET);
-        $attr = Array('image' => MATCH_PNG_GIF_JPG_FILE);
-        $this->verifyValuesSet($FHANDLER,   $attr);
-        $this->verifyValuesMatch($FHANDLER, $attr);
-        $image = $FHANDLER->get('image');
-
-        $using = Array();
-        foreach($this->CORE->getAvailableMaps() AS $map) {
-            $MAPCFG1 = new GlobalMapCfg($map);
-            try {
-                $MAPCFG1->readMapConfig();
-            } catch(MapCfgInvalid $e) {
-                continue;
-            }
-
-            foreach($MAPCFG1->getDefinitions('shape') AS $key => $obj) {
-                if(isset($obj['icon']) && $obj['icon'] == $image) {
-                    $using[] = $MAPCFG1->getName();
-                }
-            }
-        }
-        return $using;
     }
 }
 ?>
--- share/server/core/classes/ViewManageShapes.php
+++ share/server/core/classes/ViewManageShapes.php
@@ -22,36 +22,142 @@
  *
  *****************************************************************************/
 
-/**
- * @author	Lars Michelsen <lars@vertical-visions.de>
- */
 class ViewManageShapes {
-    /**
-     * Parses the information in html format
-     *
-     * @return	String 	String with Html Code
-     * @author 	Lars Michelsen <lars@vertical-visions.de>
-     */
-    public function parse() {
+    private $error = null;
+
+    private function uploadForm() {
         global $CORE;
+        echo '<h2>'.l('Upload Shape').'</h2>';
 
-        // Initialize template system
-        $TMPL = New CoreTemplateSystem($CORE);
-        $TMPLSYS = $TMPL->getTmplSys();
+        if (is_action() && post('mode') == 'upload') {
+            try {
+                if (!isset($_FILES['image']))
+                    throw new FieldInputError('image', l('You need to select an image to import.'));
 
-        $aData = Array(
-            'htmlBase'           => cfg('paths', 'htmlbase'),
-            'langUploadShape'    => l('uploadShape'),
-            'langChoosePngImage' => l('chooseImage'),
-            'langUpload'         => l('upload'),
-            'langDeleteShape'    => l('deleteShape'),
-            'langDelete'         => l('delete'),
-            'shapes'             => $CORE->getAvailableShapes(),
-            'lang'               => $CORE->getJsLang(),
-        );
+                $file = $_FILES['image'];
+                if (!is_uploaded_file($file['tmp_name']))
+                    throw new FieldInputError('image', l('The file could not be uploaded (Error: [ERROR]).',
+                      Array('ERROR' => $file['error'].': '.$CORE->getUploadErrorMsg($file['error']))));
 
-        // Build page based on the template file and the data array
-        return $TMPLSYS->get($TMPL->getTmplFile('default', 'wuiManageShapes'), $aData);
+                $file_name = $file['name'];
+                $file_path = path('sys', '', 'shapes').$file_name;
+
+                if (!preg_match(MATCH_PNG_GIF_JPG_FILE, $file_name))
+                    throw new FieldInputError('image', l('The uploaded file is no image (png,jpg,gif) file or contains unwanted chars.'));
+
+                $data = getimagesize($file['tmp_name']);
+                if (!in_array($data[2], array(IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG)))
+                    throw new FieldInputError('image', l('The uploaded file is not an image '
+                                                        .'(png, jpg and gif are allowed).'));
+
+                move_uploaded_file($file['tmp_name'], $file_path);
+                $CORE->setPerms($file_path);
+
+                success(l('The shape has been uploaded.'));
+                //reload(null, 1);
+            } catch (FieldInputError $e) {
+                form_error($e->field, $e->msg);
+            } catch (Exception $e) {
+                if (isset($e->msg))
+                    form_error(null, $e->msg);
+                else
+                    throw $e;
+            }
+        }
+        echo $this->error;
+
+        js_form_start('upload_shape');
+        hidden('mode', 'upload');
+        echo '<input type="hidden" id="MAX_FILE_SIZE" name="MAX_FILE_SIZE" value="1000000" />';
+
+        echo '<table class="mytable">';
+        echo '<tr><td class="tdlabel">'.l('Choose an Image').'</td>';
+        echo '<td class="tdfield">';
+        upload('image');
+        echo '</td></tr>';
+        echo '</table>';
+
+        submit(l('Upload'));
+        form_end();
+    }
+
+    private function deleteForm() {
+        global $CORE;
+        echo '<h2>'.l('Delete Shape').'</h2>';
+
+        if (is_action() && post('mode') == 'delete') {
+            try {
+                $name = post('name');
+                if (!$name)
+                    throw new FieldInputError('name', l('Please choose a shape'));
+
+                if (count($CORE->getAvailableShapes('/^'.preg_quote($name).'$/')) == 0)
+                    throw new FieldInputError('name', l('The shape does not exist.'));
+
+                // Check whether or not the shape is in use
+                $using = Array();
+                foreach($CORE->getAvailableMaps() AS $map) {
+                    $MAPCFG = new GlobalMapCfg($map);
+                    try {
+                        $MAPCFG->readMapConfig();
+                    } catch (Exception $e) {
+                        continue; // don't fail on broken map configs
+                    }
+
+                    foreach($MAPCFG->getDefinitions('shape') AS $key => $obj) {
+                        if(isset($obj['icon']) && $obj['icon'] == $name) {
+                            $using[] = $MAPCFG->getName();
+                        }
+                    }
+                }
+                if ($using)
+                    throw new FieldInputError('name', l('Unable to delete this shape, because it is '
+                                                       .'currently used by these maps: [M].',
+                                                            array('M' => implode(',', $using))));
+            
+                $path = path('sys', '', 'shapes', $name);
+                if ($path !== '')
+                    unlink($path);
+
+                success(l('The shape has been deleted.'));
+                //reload(null, 1);
+            } catch (FieldInputError $e) {
+                form_error($e->field, $e->msg);
+            } catch (Exception $e) {
+                if (isset($e->msg))
+                    form_error(null, $e->msg);
+                else
+                    throw $e;
+            }
+        }
+        echo $this->error;
+
+        js_form_start('delete_shape');
+        hidden('mode', 'delete');
+
+        echo '<table class="mytable">';
+        echo '<tr><td class="tdlabel">'.l('Shape').'</td>';
+        echo '<td class="tdfield">';
+        $shapes = array('' => l('Choose a shape'));
+        foreach ($CORE->getAvailableShapes() AS $name)
+            $shapes[$name] = $name;
+        select('name', $shapes);
+        echo '</td></tr>';
+
+        echo '</table>';
+
+        submit(l('Delete'));
+        form_end();
+
+    }
+
+    public function parse() {
+        ob_start();
+
+        $this->uploadForm();
+        $this->deleteForm();
+
+        return ob_get_clean();
     }
 }
 ?>
