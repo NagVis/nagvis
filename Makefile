8e5eeaae2612b2bdd3e44440ee890d50817b6b6a
Lars Michelsen <lm@larsmichelsen.com>
2017-04-28 12:19:27 +0200

    Updated changelog

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL=/bin/bash
-VERSION=1.9b16
+VERSION=1.9b17
 NAME=nagvis-$(VERSION)
 
 SED ?= sed
--- share/server/core/defines/global.php
+++ share/server/core/defines/global.php
@@ -23,7 +23,7 @@
  *****************************************************************************/
  
 // NagVis Version
-define('CONST_VERSION', '1.9b16');
+define('CONST_VERSION', '1.9b17');
 
 // Set PHP error handling to standard level
 // Different levels for php versions below 5.1 because PHP 5.1 reports
