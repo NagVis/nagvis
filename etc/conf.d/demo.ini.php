a0c3078d5be48eab6adb40869bb82678fd079c38
macarrie <mathieu0carrie@gmail.com>
2017-12-11 09:59:32 +0100

    Moved demo rotation configuration to demo.ini.php file to allow the -o option of install.sh to remove demo rotation as well

--- etc/conf.d/demo.ini.php
+++ etc/conf.d/demo.ini.php
@@ -3,9 +3,17 @@
 ; viewing this file from web.
 ; DON'T REMOVE IT!
 
-; This file defines a backend instance of the Test backend
+; This file defines the rotation object for default maps. This
+; can be removed when you are not interested in demo maps rotation.
+
+; This file also defines a backend instance of the Test backend
 ; which is used by some demo maps. This can be removed when
 ; you are not interested in the demo maps.
+
+
+[rotation_demo]
+maps="demo-germany,demo-ham-racks,demo-load,demo-muc-srv1,demo-geomap,demo-automap"
+interval=15
 
 [backend_demo]
 backendtype=Test
