528120a33b82a30b8e48f22053037bef51b96dae
Lars Michelsen <lm@larsmichelsen.com>
2023-07-11 07:58:05 +0200

    Improve error

--- share/frontend/nagvis-js/js/ViewMap.js
+++ share/frontend/nagvis-js/js/ViewMap.js
@@ -200,7 +200,7 @@ var ViewMap = View.extend({
                 obj = new NagVisLine(attrs);
             break;
             default:
-                alert('Error: Unknown object type: ' + attrs);
+                console.error('Error: Unknown object type', attrs);
                 return;
             break;
         }
