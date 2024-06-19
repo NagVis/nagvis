dff670bad222a81fd39826831a8024aac25f3f94
Michael Newton <miken32@gmail.com>
2024-06-19 14:42:34 -0600

    PSR-12: When instantiating a new class, parentheses MUST always be present

--- share/server/core/classes/GlobalCore.php
+++ share/server/core/classes/GlobalCore.php
@@ -136,7 +136,7 @@ class GlobalCore
     public static function getInstance()
     {
         if (self::$instance === null) {
-            self::$instance = new self;
+            self::$instance = new self();
         }
 
         return self::$instance;
