ee14c0d59bcb7bed5d77ecb4c845a02281e7581f
Doug Nelson <dnelson@nagios.com>
2025-07-16 11:12:10 -0500

    extend iconset_size for SVGs
    Extends iconset_size function to also resolve the image sizes of SVGs

--- share/server/core/sources/general.php
+++ share/server/core/sources/general.php
@@ -50,12 +50,49 @@ function iconset_size($iconset) {
     $fileType = $CORE->getIconsetFiletype($iconset);
     $iconPath      = path('sys',  'global', 'icons').'/'.$iconset.'_ok.'.$fileType;
     $iconPathLocal = path('sys',  'local',  'icons').'/'.$iconset.'_ok.'.$fileType;
-    if(file_exists($iconPathLocal))
-        return getimagesize($iconPathLocal);
-    elseif(file_exists($iconPath))
-        return getimagesize($iconPath);
+    if(file_exists($iconPathLocal)) {
+        if($fileType == "svg") {
+            return svg_size($iconPathLocal);
+        }
+        else {
+            return getimagesize($iconPathLocal);
+        }
+    }
+    elseif(file_exists($iconPath)){
+        if($fileType == "svg") {
+            return svg_size($iconPath);
+        }
+        else {
+            return getimagesize($iconPath);
+        }
+    }
     else
         return array(0, 0);
+}
+
+function svg_size($filepath) {
+    if (!file_exists($filepath)) {
+        return array(0, 0);
+    }
+
+    $doc = new DOMDocument();
+    $doc->load($filepath);
+
+    $svg = $doc->getElementsByTagName('svg')->item(0);
+    $width = $svg->getAttribute('width');
+    $height = $svg->getAttribute('height');
+
+    // Fallback to viewBox
+    if (empty($width) || empty($height)) {
+        $viewBox = $svg->getAttribute('viewBox');
+        $parts = preg_split('/[\s,]+/', $viewBox);
+        if (count($parts) === 4) {
+            $width = $parts[2];
+            $height = $parts[3];
+        }
+    }
+
+    return array(floatval($width), floatval($height));
 }
 
 function shape_size($icon) {
