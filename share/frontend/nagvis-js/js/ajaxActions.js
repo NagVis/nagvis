5ade96a6671598055c541c437a5e45393374ef37
AME <Armin.Radmueller@live.com>
2026-02-01 21:54:36 +0100

    fix if parseInt() interpret "auto" as NaN
    workaround if the height or width of a text box and other elements is set to auto, but parseInt() does not parse this correctly.

--- share/frontend/nagvis-js/js/ajaxActions.js
+++ share/frontend/nagvis-js/js/ajaxActions.js
@@ -6,7 +6,9 @@ function getMidOfAnchor(oObj) {
 function saveObjectAttr(objId, attr) {
     var urlPart = '';
     for (var key in attr)
-        urlPart += '&' + key + '=' + escapeUrlValues(attr[key]);
+        // parseInt() returned NaN, because value was set to "auto"
+        if ( ! isNaN(escapeUrlValues(attr[key])) )
+            urlPart += '&' + key + '=' + escapeUrlValues(attr[key]);
 
     call_ajax(oGeneralProperties.path_server + '?mod=Map&act=modifyObject&map='
               + escapeUrlValues(oPageProperties.map_name) + '&id=' + escapeUrlValues(objId) + urlPart);
